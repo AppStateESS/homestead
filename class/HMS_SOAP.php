a2186d2c32526831384d2d07fe14f07a1ddc6c63
kevin <kevin.wilcox@gmail.com>
2007-02-12 21:31:20 +0000

    + Pulled all the connection stuff into a general get_student_info() function + Added function to get student's first name + Added function to get student's middle name + Added function to get student's last name

--- class/HMS_SOAP.php
+++ class/HMS_SOAP.php
@@ -3,15 +3,39 @@
 class HMS_SOAP{
     function is_valid_student($username)
     {
-        include('SOAP/Client.php');
-        $wsdl = new SOAP_WSDL(PHPWS_SOURCE_DIR . 'mod/hms/inc/shs0001.wsdl', 'true');
-        $proxy = $wsdl->getProxy();
-        $student = $proxy->GetStudentProfile($username, '200740');
+        $student = HMS_SOAP::get_student_info($username);
         if($student->last_name == '') {
             return false;
         } else {
             return true;
         }
+    }
+
+    function get_first_name($username)
+    {
+        $student = HMS_SOAP::get_student_info($username);
+        return $student->first_name;
+    }
+
+    function get_middle_name($username)
+    {
+        $student = HMS_SOAP::get_student_info($username);
+        return $student->middle_name;
+    }
+
+    function get_last_name($username)
+    {
+        $student = HMS_SOAP::get_student_info($username);
+        return $student->last_name;
+    }
+
+    function get_student_info($username)
+    {
+        include('SOAP/Client.php');
+        $wsdl = new SOAP_WSDL(PHPWS_SOURCE_DIR . 'mod/hms/inc/shs0001.wsdl', 'true');
+        $proxy = $wsdl->getProxy();
+        $student = $proxy->GetStudentProfile($username, '200740');
+        return $student;
     }
 }
 
