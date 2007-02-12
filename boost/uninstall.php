fdd32d59e2c57fbb2d182f8d5ff0ecd914a59ed3
kevin <kevin.wilcox@gmail.com>
2007-02-12 20:19:41 +0000

    + Added hms_roommate_hashes, hms_suite to list of tables to drop

--- boost/uninstall.php
+++ boost/uninstall.php
@@ -6,15 +6,19 @@
 
 function hms_uninstall(&$content)
 {
-    $result = PHPWS_DB::dropTable('hms_student');
-    $result = PHPWS_DB::dropTable('hms_residence_hall');
+    $result = PHPWS_DB::dropTable('hms_assignment');
+    $result = PHPWS_DB::dropTable('hms_deadlines');
     $result = PHPWS_DB::dropTable('hms_floor');
-    $result = PHPWS_DB::dropTable('hms_room');
     $result = PHPWS_DB::dropTable('hms_hall_communities');
     $result = PHPWS_DB::dropTable('hms_learning_communities');
     $result = PHPWS_DB::dropTable('hms_pricing_tiers');
-    $result = PHPWS_DB::dropTable('hms_deadlines');
     $result = PHPWS_DB::dropTable('hms_questionnaire');
+    $result = PHPWS_DB::dropTable('hms_residence_hall');
+    $result = PHPWS_DB::dropTable('hms_room');
+    $result = PHPWS_DB::dropTable('hms_roommates');
+    $result = PHPWS_DB::dropTable('hms_roommate_hashes');
+    $result = PHPWS_DB::dropTable('hms_student');
+    $result = PHPWS_DB::dropTable('hms_suite');
     $content[] = _('HMS tables removed.');
     return TRUE;
 }
