af29c8af1baad31c8f7c9a4915434daf4d5a9abe
jbooker <jeremy.booker@gmail.com>
2007-02-26 20:11:11 +0000

    + Fixed spelling error in column name in table hms_learning_community_applications

--- boost/install.sql
+++ boost/install.sql
@@ -65,7 +65,7 @@ CREATE TABLE hms_learning_community_applications (
     rlc_first_choice_id             integer NOT NULL REFERENCES hms_learning_communities(id),
     rlc_second_choice_id            integer NOT NULL REFERENCES hms_learning_communities(id),
     rlc_third_choice_id             integer NOT NULL REFERENCES hms_learning_communities(id),
-    why_specific_communitites       character varying(500) NOT NULL,
+    why_specific_communities        character varying(500) NOT NULL,
     strengths_weaknesses            character varying(500) NOT NULL,
     rlc_question_0                  character varying(500),
     rlc_question_1                  character varying(500),
