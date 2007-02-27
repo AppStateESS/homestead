55cda1b73f0394a1ea44d9a191cc256af00d5060
kevin <kevin.wilcox@gmail.com>
2007-02-27 23:54:45 +0000

    * Changed order so older updates would be processed.

--- boost/update.php
+++ boost/update.php
@@ -7,6 +7,28 @@
 function hms_update(&$content, $currentVersion)
 {
     switch ($currentVersion) {
+        case version_compare($currentVersion, '0.1.2', '<'):
+            $db = & new PHPWS_DB;
+            $result = $db->importFile(PHPWS_SOURCE_DIR . 'mod/hms/boost/0_1_2.sql');
+            if (PEAR::isError($result)) {
+                return $result;
+            }
+            
+            $files = array();
+            $files[] = 'templates/student/rlc_signup_form_page2.tmp';
+            
+            PHPWS_Boost::updateFiles($files, 'hms');
+            
+            $content[] = _('+ RLC application form template');
+            $content[] = _('+ RLC application table');
+        
+        case version_compare($currentVersion, '0.1.3', '<'):
+            $files = array();
+            $files[] = 'templates/student/rlc_signup_confirmation.tpl';
+            PHPWS_Boost::updateFiles($files, 'hms');
+
+            $content[] = _('+ Complete system for RLC applications');
+        
         case version_compare($currentVersion, '0.1.4', '<'):
             $db = & new PHPWS_DB;
             $result = $db->importFile(PHPWS_SOURCE_DIR . 'mod/hms/boost/0_1_4.sql');
@@ -27,28 +49,7 @@ function hms_update(&$content, $currentVersion)
             $content[] = _('+ Deadline for Questionnaire replaced by deadlines for Profile and Application');
             $content[] = _('+ Deadline added for editing applications');
             $content[] = _('+ Deadline added for submitting RLC applications'); 
-        
-        case version_compare($currentVersion, '0.1.3', '<'):
-            $files = array();
-            $files[] = 'templates/student/rlc_signup_confirmation.tpl';
-            PHPWS_Boost::updateFiles($files, 'hms');
 
-            $content[] = _('+ Complete system for RLC applications');
-
-        case version_compare($currentVersion, '0.1.2', '<'):
-            $db = & new PHPWS_DB;
-            $result = $db->importFile(PHPWS_SOURCE_DIR . 'mod/hms/boost/0_1_2.sql');
-            if (PEAR::isError($result)) {
-                return $result;
-            }
-            
-            $files = array();
-            $files[] = 'templates/student/rlc_signup_form_page2.tmp';
-            
-            PHPWS_Boost::updateFiles($files, 'hms');
-            
-            $content[] = _('+ RLC application form template');
-            $content[] = _('+ RLC application table');
     }
 
     return TRUE;
