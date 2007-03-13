ffda301f4ec0421c17513c98cf7ffe267f7648b7
kevin <kevin.wilcox@gmail.com>
2007-03-13 15:44:11 +0000

    + Changed hms_room.room_number to a character varying field instead of smallint. *   Allows for Ground Floor room numbering (00x)

--- boost/0_1_8.sql
+++ boost/0_1_8.sql
@@ -1,2 +1,10 @@
 ALTER TABLE hms_residence_hall ADD COLUMN numbering_scheme smallint;
 ALTER TABLE hms_residence_hall ALTER COLUMN numbering_scheme SET NOT NULL;
+
+ALTER TABLE hms_room ADD COLUMN temp_number character varying(5);
+UPDATE hms_room SET temp_number = room_number;
+ALTER TABLE hms_room DROP COLUMN room_number;
+ALTER TABLE hms_room ADD COLUMN room_number character varying(5);
+UPDATE hms_room SET room_number = temp_number;
+ALTER TABLE hms_room DROP COLUMN temp_number;
+
