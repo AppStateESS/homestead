1f64a676a4d709a87628f86cc13a2f0a5cdd507c
jbooker <jeremy.booker@gmail.com>
2007-03-14 17:08:07 +0000

    + Added inital inserts for learning communities and their corresponding questions to install.sql.

--- boost/install.sql
+++ boost/install.sql
@@ -239,3 +239,25 @@ CREATE TABLE hms_beds (
     bed_letter character(1) NOT NULL,
     PRIMARY KEY(id)
 );
+
+
+INSERT INTO hms_learning_communities (id, community_name, abbreviation, capacity) VALUES (0, 'Leadership & Service Community', 'LSC', 50);
+INSERT INTO hms_learning_communities (id, community_name, abbreviation, capacity) VALUES (1, 'Outdoor Community', 'OC', 50);
+INSERT INTO hms_learning_communities (id, community_name, abbreviation, capacity) VALUES (2, 'Wellness Community', 'WC', 50);
+INSERT INTO hms_learning_communities (id, community_name, abbreviation, capacity) VALUES (3, 'Community of Scientific Interest', 'CSI', 50);
+INSERT INTO hms_learning_communities (id, community_name, abbreviation, capacity) VALUES (4, 'Language & Culture Community', 'LCC', 50);
+INSERT INTO hms_learning_communities (id, community_name, abbreviation, capacity) VALUES (5, 'Black & Gold Community', 'BGC', 50);
+
+CREATE SEQUENCE hms_learning_communities_seq;
+SELECT setval('hms_learning_communities_seq', max(hms_learning_communities.id));
+
+
+INSERT INTO hms_learning_community_questions (id, learning_community_id, question_text) VALUES (0, 0, 'Describe your current leadership and community service experience and the opportunities you are looking for.');
+INSERT INTO hms_learning_community_questions (id, learning_community_id, question_text) VALUES (1, 1, 'What outdoor opportunities would you like to be involved in and describe your current experience.');
+INSERT INTO hms_learning_community_questions (id, learning_community_id, question_text) VALUES (2, 2, 'What are your personal experienes with wellness and in what areas of wellness are you most interested?');
+INSERT INTO hms_learning_community_questions (id, learning_community_id, question_text) VALUES (3, 3, 'What knowledge, skills, or talent could you offer other students in the Community of Scientific Interests?');
+INSERT INTO hms_learning_community_questions (id, learning_community_id, question_text) VALUES (4, 4, 'In what languages are you proficient, learning to speak, or interested in learning?');
+INSERT INTO hms_learning_community_questions (id, learning_community_id, question_text) VALUES (5, 5, 'How do you plan to be an active member of the ASU community?');
+
+CREATE SEQUENCE hms_learning_community_questions_seq;
+SELECT setval('hms_learning_community_questions_seq', max(hms_learning_community_questions.id));
