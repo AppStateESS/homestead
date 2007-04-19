c07ed3db737479e69d543fcf880681dc177551fd
kevin <kevin.wilcox@gmail.com>
2007-04-19 18:47:47 +0000

    + Welcome to 0.1.14

--- boost/boost.php
+++ boost/boost.php
@@ -1,6 +1,6 @@
 <?php
     $proper_name  = 'Housing Management System';
-    $version      = '0.1.13';
+    $version      = '0.1.14';
     $import_sql   = TRUE;
     $image_dir    = TRUE;
     $file_dir     = FALSE;
