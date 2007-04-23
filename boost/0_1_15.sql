3af3904f9b58785c8ad425a79ae5741d4d23e995
kevin <kevin.wilcox@gmail.com>
2007-04-23 18:03:45 +0000

    - Removed hms_roommate_hashes + Added hms_roommate_approval

--- boost/0_1_15.sql
+++ boost/0_1_15.sql
@@ -1,1 +1,18 @@
 ALTER TABLE add constraint hms_student_profile_user UNIQUE user_id;
+
+DROP TABLE hms_roommate_hashes;
+CREATE TABLE hms_roommate_approval (
+    id INTEGER NOT NULL,
+    room_id INTEGER NOT NULL,
+    number_roommates SMALLINT NOT NULL,
+    approval_hash CHARACTER VARYING(64) NOT NULL,
+    roommate_zero CHARACTER VARYING(32) NOT NULL,
+    roommate_zero_approved SMALLINT NOT NULL,
+    roommate_one CHARACTER VARYING(32) NOT NULL,
+    roommate_one_approved SMALLINT NOT NULL,
+    roommate_two CHARACTER VARYING(32),
+    roommate_two_approved SMALLINT,
+    roommate_three CHARACTER VARYING(32),
+    roommate_three_approved SMALLINT,
+    PRIMARY KEY (id)
+);
