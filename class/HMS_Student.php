648b0fcd726c4ff01b200dd00a3531e6c4b66099
kevin <kevin.wilcox@gmail.com>
2007-05-01 20:04:10 +0000

    * Moved the code to display the main student menu to it's own show_main_menu() method

--- class/HMS_Student.php
+++ class/HMS_Student.php
@@ -260,6 +260,101 @@ class HMS_Student {
         return $content;
     }
 
+    function show_main_menu()
+    {
+        PHPWS_Core::initModClass('hms', 'HMS_Application.php');
+        if(HMS_Application::check_for_application($_SESSION['asu_username'])) {
+            PHPWS_Core::initModClass('hms', 'HMS_Side_Thingie.php');
+            // TODO: This will have to check whether a profile has been made
+            // or not, and then whether a roommate has been selected or not:
+            // if not profile, then HMS_SIDE_STUDENT_PROFILE
+            // if not roommate, then HMS_SIDE_STUDENT_ROOMMATE
+            // else, HMS_SIDE_STUDENT_VERIFY
+            $side_thingie = new HMS_Side_Thingie(HMS_SIDE_STUDENT_PROFILE);
+            $side_thingie->show();
+
+            $message  = "Welcome to the Housing Management System!<br /><br />";
+            $message .= "You have already completed a Housing Application. You may click below to review it.<br /><br />";
+            $message .= "You may also submit a new application. This will replace the one you already have saved.<br /><br />";
+            $message .= PHPWS_Text::secureLink(_('View My Application'), 'hms', array('type'=>'student', 'op'=>'review_application'));
+            $message .= "<br /><br />";
+            $message .= PHPWS_Text::secureLink(_('Submit New Application'), 'hms', array('type'=>'student', 'op'=>'begin_application'));
+            $message .= "<br /><br />";
+            
+            PHPWS_Core::initModClass('hms','HMS_RLC_Application.php');
+            if(HMS_RLC_Application::check_for_application() === FALSE){
+                $message .= PHPWS_Text::secureLink(_('RLC Application Form'), 'hms', array('type'=>'student', 'op'=>'show_rlc_application_form'));
+            } else {
+                $message .= PHPWS_Text::secureLink(_('View My RLC Application'), 'hms', array('type'=>'student', 'op'=>'view_rlc_application'));
+            }
+            $message .= "<br /><br />";
+    
+            PHPWS_Core::initModClass('hms', 'HMS_Student_Profile.php');
+            if(HMS_Student_Profile::check_for_profile() === FALSE) {
+                $message .= PHPWS_Text::secureLink(_('Create your Profile'), 'hms', array('type'=>'student', 'op' =>'show_profile_form'));
+            } else {
+                $message .= "You have created a profile. You should be able to edit and view this profile by Monday, 7 May, 2007.";
+            }
+            $message .= "<br /><br />";
+
+            PHPWS_Core::initModClass('hms', 'HMS_Roommate_Approval.php');
+            if(HMS_Roommate_Approval::has_requested_someone($_SESSION['asu_username'])) {
+                $message .= "You have selected a roommate and are awaiting their approval.";
+            } else {
+                $message .= "If you know who you want to room with, you can go ahead and " . PHPWS_Text::secureLink(_('select your roommate.'), 'hms', array('type'=>'student','op'=>'get_roommate_username'));
+            }
+
+            $message .= "<br /><br />";
+            $message .= "If you do not know who you want to room with, our roommate search tool should be available by Monday, 7 May, 2007.";
+            //$message .= PHPWS_Text::secureLink('roommate search tool.', 'hms', array('type'=>'student','op'=>'show_application_search'));
+            $message .= "<br /><br />";
+            $message .= PHPWS_Text::secureLink(_('Logout'), 'users', array('action'=>'user', 'command'=>'logout'));
+            $message .= "<br /><br />";
+        } else {
+            PHPWS_Core::initModClass('hms', 'HMS_Side_Thingie.php');
+            $side_thingie = new HMS_Side_Thingie(HMS_SIDE_STUDENT_AGREE);
+            $side_thingie->show();
+           
+            $form = new PHPWS_Form;
+            $form->addHidden('module', 'hms');
+            $form->addHidden('type', 'student');
+            $form->addHidden('op', 'begin_application');
+            $form->addSubmit('begin', _('I AGREE'));
+            $form->addSubmit('quit', _('I DISAGREE'));
+            
+            $message  = "<b>Please read the following License Agreement and click either 'I AGREE' or 'I DISAGREE'<br />";
+            $message .= 'Please note that if you click disagree you will be logged out of HMS.</b><br /><br />';
+            $message .= 'If you wish to read this Agreement as a printable PDF please ';
+            $message .= '<a href="http://hms.appstate.edu/files/contract.pdf" target="_blank">click here.</a><br /><br />';
+            $message .= 'If you need to update or download a PDF viewer you can <a href="http://www.adobe.com/products/acrobat/readstep2.html" target="_blank">get one here</a><br /><br />';
+
+            # Check for under 18, display link to print message
+            PHPWS_Core::initModClass('hms','HMS_SOAP.php');
+            $dob = explode('-', HMS_SOAP::get_dob($_REQUEST['asu_username']));
+            $dob_timestamp = mktime(0,0,0,$dob[1],$dob[2],$dob[0]);
+            $current_timestamp = mktime(0,0,0);
+            if(($current_timestamp - $dob_timestamp) < (3600 * 24 * 365 * 18)){
+                $message .= '<br /><font color="red">Because you are under age 18, you MUST print a copy of the Housing Contract Agreement, ';
+                $message .= 'have a parent or legal guardian sign it, and return it to the Department of ';
+                $message .= 'Housing and Residence Life. Your application cannot be fully processed until a Housing Contract ';
+                $message .= 'signed by a parent or gaurdian is on file. Please <a href="http://hms.appstate.edu/files/contract.pdf">click here </a>';
+                $message .= 'to open a printer-friendly version of the Housing Contract.</font><br /><br />';
+
+                # Set the 'agreed_to_terms' flag to false
+                $form->addHidden('agreed_to_terms',0);
+            }else{
+                $form->addHidden('agreed_to_terms',1);
+            }
+            
+            $tpl = $form->getTemplate();
+
+            $tpl['MESSAGE'] = $message;
+            $tpl['CONTRACT'] = str_replace("\n", "<br />", file_get_contents('mod/hms/inc/contract.txt'));
+            
+            $message = PHPWS_Template::process($tpl, 'hms', 'student/contract.tpl');
+        }       
+        return $message;
+    }
     function main()
     {
         if($_REQUEST['op'] == 'login') {
@@ -369,88 +464,18 @@ class HMS_Student {
             case 'verify_roommate_username':
                 return HMS_Student::verify_roommate_username();
                 break;
-            case 'main':
-                PHPWS_Core::initModClass('hms', 'HMS_Application.php');
-                if(HMS_Application::check_for_application($_SESSION['asu_username'])) {
-                    PHPWS_Core::initModClass('hms', 'HMS_Side_Thingie.php');
-                    // TODO: This will have to check whether a profile has been made
-                    // or not, and then whether a roommate has been selected or not:
-                    // if not profile, then HMS_SIDE_STUDENT_PROFILE
-                    // if not roommate, then HMS_SIDE_STUDENT_ROOMMATE
-                    // else, HMS_SIDE_STUDENT_VERIFY
-                    $side_thingie = new HMS_Side_Thingie(HMS_SIDE_STUDENT_PROFILE);
-                    $side_thingie->show();
-
-                    $message  = "Welcome to the Housing Management System!<br /><br />";
-                    $message .= "You have already completed a Housing Application. You may click below to review it.<br /><br />";
-                    $message .= "You may also submit a new application. This will replace the one you already have saved.<br /><br />";
-                    $message .= PHPWS_Text::secureLink(_('View My Application'), 'hms', array('type'=>'student', 'op'=>'review_application'));
-                    $message .= "<br /><br />";
-                    $message .= PHPWS_Text::secureLink(_('Submit New Application'), 'hms', array('type'=>'student', 'op'=>'begin_application'));
-                    $message .= "<br /><br />";
-                    
-                    PHPWS_Core::initModClass('hms','HMS_RLC_Application.php');
-                    if(HMS_RLC_Application::check_for_application() === FALSE){
-                        $message .= PHPWS_Text::secureLink(_('RLC Application Form'), 'hms', array('type'=>'student', 'op'=>'show_rlc_application_form'));
-                        $message .="<br /><br />";
-                    } else {
-                        $message .= PHPWS_Text::secureLink(_('View My RLC Application'), 'hms', array('type'=>'student', 'op'=>'view_rlc_application'));
-                        $message .= "<br /><br />";
-                    }
-
-                    $message .= PHPWS_Text::secureLink(_('Create your Profile'), 'hms', array('type'=>'student', 'op' =>'show_profile_form'));
-                    $message .= "<br /><br />";
-                    $message .= PHPWS_Text::secureLink(_('Select your roommate'), 'hms', array('type'=>'student','op'=>'get_roommate_username'));
-                    $message .= "<br /><br />";
-                    $message .= "If you do not know who you want to room with, please use our ";
-                    $message .= PHPWS_Text::secureLink('roommate search tool.', 'hms', array('type'=>'student','op'=>'show_application_search'));
-                    $message .= "<br /><br />";
-                    $message .= PHPWS_Text::secureLink(_('Logout'), 'users', array('action'=>'user', 'command'=>'logout'));
-                    $message .= "<br /><br />";
+            case 'save_roommate_username':
+                if(isset($_REQUEST['cancel'])) {
+                    return HMS_Student::get_roommate_username();
                 } else {
-                    PHPWS_Core::initModClass('hms', 'HMS_Side_Thingie.php');
-                    $side_thingie = new HMS_Side_Thingie(HMS_SIDE_STUDENT_AGREE);
-                    $side_thingie->show();
-                   
-                    $form = new PHPWS_Form;
-                    $form->addHidden('module', 'hms');
-                    $form->addHidden('type', 'student');
-                    $form->addHidden('op', 'begin_application');
-                    $form->addSubmit('begin', _('I AGREE'));
-                    $form->addSubmit('quit', _('I DISAGREE'));
-                    
-                    $message  = "<b>Please read the following License Agreement and click either 'I AGREE' or 'I DISAGREE'<br />";
-                    $message .= 'Please note that if you click disagree you will be logged out of HMS.</b><br /><br />';
-                    $message .= 'If you wish to read this Agreement as a printable PDF please ';
-                    $message .= '<a href="http://hms.appstate.edu/files/contract.pdf" target="_blank">click here.</a><br /><br />';
-                    $message .= 'If you need to update or download a PDF viewer you can <a href="http://www.adobe.com/products/acrobat/readstep2.html" target="_blank">get one here</a><br /><br />';
-
-                    # Check for under 18, display link to print message
-                    PHPWS_Core::initModClass('hms','HMS_SOAP.php');
-                    $dob = explode('-', HMS_SOAP::get_dob($_REQUEST['asu_username']));
-                    $dob_timestamp = mktime(0,0,0,$dob[1],$dob[2],$dob[0]);
-                    $current_timestamp = mktime(0,0,0);
-                    if(($current_timestamp - $dob_timestamp) < (3600 * 24 * 365 * 18)){
-                        $message .= '<br /><font color="red">Because you are under age 18, you MUST print a copy of the Housing Contract Agreement, ';
-                        $message .= 'have a parent or legal guardian sign it, and return it to the Department of ';
-                        $message .= 'Housing and Residence Life. Your application cannot be fully processed until a Housing Contract ';
-                        $message .= 'signed by a parent or gaurdian is on file. Please <a href="http://hms.appstate.edu/files/contract.pdf">click here </a>';
-                        $message .= 'to open a printer-friendly version of the Housing Contract.</font><br /><br />';
-
-                        # Set the 'agreed_to_terms' flag to false
-                        $form->addHidden('agreed_to_terms',0);
-                    }else{
-                        $form->addHidden('agreed_to_terms',1);
-                    }
-                    
-                    $tpl = $form->getTemplate();
-
-                    $tpl['MESSAGE'] = $message;
-                    $tpl['CONTRACT'] = str_replace("\n", "<br />", file_get_contents('mod/hms/inc/contract.txt'));
-                    
-                    $message = PHPWS_Template::process($tpl, 'hms', 'student/contract.tpl');
-                }       
-                return $message;
+                    PHPWS_Core::initModClass('hms', 'HMS_Roommate_Approval.php');
+                    return HMS_Roommate_Approval::save_roommate_username();
+                }
+                break;
+            case 'main':
+                return HMS_Student::show_main_menu();
+                break;
+               return $message;
             default:
                 return "{$_REQUEST['op']} <br />";
                 break;
