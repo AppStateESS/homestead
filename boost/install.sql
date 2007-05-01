53d5f7caecf6191c363c28b8734c42a22a304d86
kevin <kevin.wilcox@gmail.com>
2007-05-01 20:08:38 +0000

    - Removed room_id - Removed the generic approval hash + Added personalized hashes

--- boost/install.sql
+++ boost/install.sql
@@ -182,17 +182,19 @@ CREATE TABLE hms_roommates (
 
 CREATE TABLE hms_roommate_approval (
     id INTEGER NOT NULL,
-    room_id INTEGER NOT NULL,
     number_roommates SMALLINT NOT NULL,
-    approval_hash CHARACTER VARYING(64) NOT NULL,
     roommate_zero CHARACTER VARYING(32) NOT NULL,
     roommate_zero_approved SMALLINT NOT NULL,
+    roommate_zero_personal_hash CHARACTER VARYING(32) NOT NULL,
     roommate_one CHARACTER VARYING(32) NOT NULL,
     roommate_one_approved SMALLINT NOT NULL,
+    roommate_one_personal_hash CHARACTER VARYING(32) NOT NULL,
     roommate_two CHARACTER VARYING(32),
     roommate_two_approved SMALLINT,
+    roommate_two_personal_hash CHARACTER VARYING(32),
     roommate_three CHARACTER VARYING(32),
     roommate_three_approved SMALLINT,
+    roommate_three_personal_hash CHARACTER VARYING(32),
     PRIMARY KEY (id)
 );
 
