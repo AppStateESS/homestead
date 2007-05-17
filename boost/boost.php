a09b8869cbb0488b3e64fe449f0b389b8d1a82ec
kevin <kevin.wilcox@gmail.com>
2007-05-17 05:24:22 +0000

    + Up to version 0.1.20

--- boost/boost.php
+++ boost/boost.php
@@ -1,6 +1,6 @@
 <?php
     $proper_name  = 'Housing Management System';
-    $version      = '0.1.19';
+    $version      = '0.1.20';
     $import_sql   = TRUE;
     $image_dir    = TRUE;
     $file_dir     = FALSE;
