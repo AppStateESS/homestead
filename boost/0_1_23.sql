b3a5e068b9a8ffb6208b0a7e1afe68e535b81377
kevin <kevin.wilcox@gmail.com>
2007-06-01 17:12:00 +0000

    * Yikes, forgot to fix a bug!

--- boost/0_1_23.sql
+++ boost/0_1_23.sql
@@ -4,3 +4,6 @@ ALTER TABLE hms_learning_community_assignment DROP COLUMN asu_username;
 ALTER TABLE hms_learning_community_assignment ADD COLUMN asu_username character varying(32);
 UPDATE hms_learning_community_assignment SET asu_username = temp_name;
 ALTER TABLE hms_learning_community_assignment DROP COLUMN temp_name;
+
+ALTER TABLE hms_room ADD COLUMN displayed_room_number character varying(8);
+UPDATE hms_room SET displayed_room_number = room_number;
