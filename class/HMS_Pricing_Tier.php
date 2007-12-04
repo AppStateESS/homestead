f72b6f48beb2b598ae4e4e0f4f3c958bb71b8900
jbooker <jeremy.booker@gmail.com>
2007-12-04 22:23:47 +0000

    + Added the HMS_Pricing_Tier class.

---
+++ class/HMS_Pricing_Tier.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * HMS Pricing Tier class
+ *
+ * @author Jeremy Booker <jbooker at tux dot appstate dot edu>
+ */
+
+class HMS_Pricing_Tier
+{
+
+    var $id             = 0;
+    var $tier_value     = null;
+
+    function HMS_Pricing_Tier($id = 0)
+    {
+        if (!$id) {
+            return;
+        }
+
+        $this->id = $id;
+        $db = new PHPWS_DB('hms_pricing_tiers');
+        $db->addWhere('id', $this->id);
+        $result = $db->loadObject($this);
+        if (!$result || PHPWS_Error::logIfError($result)) {
+            $this->id = 0;
+        }
+    }
+
+    /*******************
+    * Static Functions *
+    *******************/
+
+    function get_pricing_tiers()
+    {
+        $db = new PHPWS_DB('hms_pricing_tiers');
+        $db->addOrder('tier_value', 'ASC');
+
+        $db->loadClass('hms', 'HMS_Pricing_Tier.php');
+        $result = $db->getObjects('HMS_Pricing_Tier');
+
+        if(PHPWS_Error::logIfError($result)){
+            return false;
+        } else {
+            return $result;
+        }
+    }
+
+    function get_pricing_tiers_array()
+    {
+        $tiers_array = array();
+        
+        $tiers = HMS_Pricing_Tier::get_pricing_tiers();
+
+        foreach ($tiers as $tier){
+            $tiers_array[$tier->id] = $tier->tier_value;
+        }
+
+        return $tiers_array;
+    }
+}
+
+?>
