56d90631234befeab5995abf77ad02e5914b9d85
jbooker <jeremy.booker@gmail.com>
2007-12-12 18:30:10 +0000

    + Added the applications_to_rereport file and made a bit of progress on the script

--- util/rereport_applications.php
+++ util/rereport_applications.php
@@ -1,11 +1,27 @@
 #!/usr/bin/php
 <?php
 
+/**
+ * Rereport HMS Applications to Banner
+ *
+ * This script was designed to fix a case where HMS has
+ * saved student housing applications, but those applications
+ * do not appear in Banner.
+ */
+
 define('PHPWS_SOURCE_DIR', '../../../');
-define('SOAP_TEST_FLAG', FALSE);
 
 require('../class/HMS_SOAP.php');
 
-echo "Rereporting Applications";
+echo "Rereporting Applications...\n";
+
+# Get the list of usernames
+$usernames = file('applications_to_rereport-2007-12-12.txt');
+
+echo sizeof($usernames) . ' applications to report...';
+
+# Connect to the database
+$db = pg_connect();
+
 
 ?>
