2cfa7016a182a8420d27624e9d9a8f44ccb212f5
dwest <westwolf24@gmail.com>
2008-02-13 21:16:54 +0000

    Changed hms_deadlines in the install.sql file to include the lottery dealines.

--- boost/install.sql
+++ boost/install.sql
@@ -160,6 +160,8 @@ CREATE TABLE hms_deadlines (
     submit_rlc_application_end_timestamp    integer NOT NULL,
     view_assignment_begin_timestamp         integer NOT NULL,
     view_assignment_end_timestamp           integer NOT NULL,
+    lottery_signup_begin_timestamp          integer NOT NULL,
+    lottery_signup_end_timestamp            integer NOT NULL,
     updated_by                              smallint NOT NULL,
     updated_on                              integer NOT NULL,
     primary key(id)
