c5cc8f874888f2204f40add9917cffa0f400d759
dwest <westwolf24@gmail.com>
2008-03-18 17:08:53 +0000

    Fixed ticket #36, installer now creates necessary local users.

---
+++ boost/install.php
@@ -0,0 +1,25 @@
+<?php
+
+/************************************************************/
+/* Install file for hms, creates local users for the module */
+/*                                                          */
+/* @author Daniel West                                      */
+/* @package mod                                             */
+/* @subpackage hms                                          */
+/************************************************************/
+
+function hms_install(&$content)
+{
+   PHPWS_Core::initModClass('users', 'Users.php');
+   $user = new PHPWS_User(); 
+   $user->setUsername('hms_admin');
+   $user->setPassword('in the white room, with black curtains');
+   $user->save();
+
+   $user = new PHPWS_User();
+   $user->setUsername('hms_student');
+   $user->setPassword('shes my everything, shes my pride and joy');
+   $user->save();
+
+   return true;
+}
