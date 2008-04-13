72108507ba3d5e2b0cb1d14a7f278cf179485d75
jbooker <jeremy.booker@gmail.com>
2008-04-13 01:46:49 +0000

    + Fixed student profile searching so that if a student is assigned to an rlc then the search will only return other students in the same rlc * Fixes Trac #24

--- class/HMS_Student_Profile.php
+++ class/HMS_Student_Profile.php
@@ -637,13 +637,81 @@ class HMS_Student_Profile{
 
 
     /**
-     * Uses the forms class to display the profile search page.
+     * Display the profile search page.
      */
     function display_profile_search()
     {
-        PHPWS_Core::initModClass('hms', 'HMS_Forms.php');
         $_SESSION['profile_search_use_session'] = FALSE;
-        return HMS_Form::profile_search_form();
+        require_once(PHPWS_SOURCE_DIR . 'mod/hms/inc/profile_options.php');
+
+        # Overwrite the first element of each array so it reads "No Preference" instead of "Prefer not to say".
+        $no_pref = 'No Preference';
+        $political_views[0]  = $no_pref;
+        $majors[0]           = $no_pref;
+        $experiences[0]      = $no_pref;
+        $sleep_times[0]      = $no_pref;
+        $wakeup_times[0]     = $no_pref;
+        $overnight_guests[0] = $no_pref;
+        $loudness[0]         = $no_pref;
+        $cleanliness[0]      = $no_pref;
+        $free_time[0]        = $no_pref;
+
+        $tags = array();
+
+        $form = &new PHPWS_Form();
+        $form->setAction('index.php?module=hms&type=student&op=profile_search');
+        $form->useRowRepeat();
+
+        $form->addText('asu_username');
+        $form->setLabel('asu_username','ASU Username: ');
+
+        $form->addCheck('hobbies_checkbox',$hobbies);
+        $form->setLabel('hobbies_checkbox',$hobbies_labels);
+        $tags['HOBBIES_CHECKBOX_QUESTION'] = 'Hobbies and Interests (check all that apply):';
+
+        $form->addCheck('music_checkbox',$music);
+        $form->setLabel('music_checkbox',$music_labels);
+        $tags['MUSIC_CHECKBOX_QUESTION'] = 'Music Preferences (check all that apply):';
+
+        $form->addDropBox('political_views_dropbox',$political_views);
+        $form->setLabel('political_views_dropbox','His/her political view: ');
+
+        /***** College Life *****/
+        $form->addDropBox('intended_major',$majors);
+        $form->setLabel('intended_major','His/her academic major: ');
+
+        $form->addDropBox('important_experience',$experiences);
+        $form->setLabel('important_experience','The following is the most important part of his/her college experience: ');
+
+        /***** Daily Life *****/
+        $form->addDropBox('sleep_time',$sleep_times);
+        $form->setLabel('sleep_time','He/she generally goes to sleep: ');
+
+        $form->addDropBox('wakeup_time',$wakeup_times);
+        $form->setLabel('wakeup_time','He/she generally wakes up: ');
+
+        $form->addDropBox('overnight_guests',$overnight_guests);
+        $form->setLabel('overnight_guests','He/she plans on hosting overnight guests: ');
+        
+        $form->addDropBox('loudness',$loudness);
+        $form->setLabel('loudness','In his/her daily activities (music, conversations, etc.): ');
+
+        $form->addDropBox('cleanliness',$cleanliness);
+        $form->setLabel('cleanliness','He/she could be described as: ');
+
+        $tags['STUDY_TIMES_QUESTION'] = 'He/she prefers to study (check all that apply):';
+        $form->addCheck('study_times',$study_times);
+        $form->setLabel('study_times',$study_times_labels);
+        
+        $form->addDropBox('free_time',$free_time);
+        $form->setLabel('free_time','If he/she has free time he/she would rather: ');
+        
+        $form->addSubmit('Search');
+
+        $form->mergeTemplate($tags);
+        $tags = $form->getTemplate();
+
+        return PHPWS_Template::process($tags,'hms','student/profile_search.tpl');
     }
 
     /**
@@ -665,6 +733,8 @@ class HMS_Student_Profile{
     {
         # get the current student's gender
         PHPWS_Core::initModClass('hms','HMS_SOAP.php');
+        PHPWS_Core::initModClass('hms','HMS_RLC_Assignment.php');
+
         $gender = HMS_SOAP::get_gender($_SESSION['asu_username'], TRUE); # use the numeric form
         
         PHPWS_Core::initCoreClass('DBPager.php');
@@ -675,6 +745,16 @@ class HMS_Student_Profile{
         $pageTags['ACTIONS']    = _('Action');
 
         $pager = &new DBPager('hms_student_profiles','HMS_Student_Profile');
+
+        // Check to see if user is assigned to an RLC
+        $rlc_assignment = HMS_RLC_Assignment::check_for_assignment($_SESSION['asu_username'], $_SESSION['application_term']);
+        if($rlc_assignment != FALSE){
+            // User is assigned to an RLC, only show results from other students in the same RLC
+            $pager->db->addJoin('LEFT OUTER', 'hms_student_profiles', 'hms_learning_community_applications', 'user_id', 'user_id');
+            $pager->db->addJoin('LEFT OUTER', 'hms_learning_community_applications', 'hms_learning_community_assignment', 'hms_assignment_id', 'id');
+            $pager->db->addWhere('hms_learning_community_assignment.rlc_id', $rlc_assignment['rlc_id']);
+            //$pager->db->setTestMode();
+        }
 
         # If the $_SESSION variable is set, and set to true, then use the session, otherwise pull from $_REQUEST and create $_SESSION
         if(isset($_SESSION['profile_search_use_session']) && $_SESSION['profile_search_use_session'] == TRUE){
@@ -1252,7 +1332,8 @@ class HMS_Student_Profile{
         }
         
         # Join with hms_application table on user_id to make sure genders match.
-        $pager->addWhere('hms_student_profiles.user_id','hms_application.hms_student_id','ILIKE');
+        $pager->db->addJoin('LEFT OUTER', 'hms_student_profiles', 'hms_application', 'user_id', 'hms_student_id');
+        //$pager->addWhere('hms_student_profiles.user_id','hms_application.hms_student_id','ILIKE');
         $pager->addWhere('hms_application.gender',$gender,'=');
 
         # Don't list the current user as a match
