4591ae4b31155acb9165cbe9d79140187683b972
matt <mcnaney@gmail.com>
2008-05-14 17:13:15 +0000

    jtickle: added assignments and suites to the deletion

--- boost/0_2_33.sql
+++ boost/0_2_33.sql
@@ -1,7 +1,10 @@
 BEGIN;
 UPDATE hms_floor SET deleted = 1 WHERE hms_residence_hall.deleted = 1 AND hms_residence_hall.id = hms_floor.residence_hall_id;
+UPDATE hms_suite SET deleted = 1 WHERE hms_floor.deleted = 1 AND hms_suite.floor_id = hms_floor.id;
 UPDATE hms_room SET deleted = 1 WHERE hms_floor.deleted = 1 AND hms_floor.id = hms_room.floor_id;
 UPDATE hms_bed SET deleted = 1 WHERE hms_room.deleted = 1 AND hms_room.id = hms_bed.room_id;
+UPDATE hms_assignment SET deleted = 1 WHERE hms_bed.deleted = 1 AND hms_bed.id = hms_assignment.bed_id;
+DELETE FROM hms_assignment WHERE deleted = 1;
 DELETE from hms_bed where deleted = 1;
 DELETE from hms_room where deleted = 1;
 DELETE from hms_floor where deleted = 1;
