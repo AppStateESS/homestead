e4c37f992d3956aa1044647ded11a787fed15029
matt <mcnaney@gmail.com>
2008-05-14 17:02:02 +0000

    Version 33 lawl

--- boost/boost.php
+++ boost/boost.php
@@ -1,6 +1,6 @@
 <?php
     $proper_name  = 'Housing Management System';
-    $version      = '0.2.32';
+    $version      = '0.2.33';
     $import_sql   = TRUE;
     $image_dir    = TRUE;
     $file_dir     = FALSE;
