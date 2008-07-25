148737ba0a284715903ec077f4172e1200e68fbc
matt <mcnaney@gmail.com>
2008-07-25 12:52:55 +0000

    Dear jbooker, you missed a parenthesis.  <3, jtickle

--- class/HMS_Maintenance.php
+++ class/HMS_Maintenance.php
@@ -127,7 +127,7 @@ class HMS_Maintenance
         if(Current_User::allow('hms', 'edit_terms'))
             $tpl['CREATE_TERM'] = PHPWS_Text::secureLink(_('Create a New Term'), 'hms', array('type'=>'term', 'op'=>'show_create_term'));
         
-        if(Current_User::allow('hms', 'select_term') || Current_User::allow('hms', 'edit_terms')
+        if(Current_User::allow('hms', 'select_term') || Current_User::allow('hms', 'edit_terms'))
             $tpl['EDIT_TERM']   = PHPWS_Text::secureLink(_('Edit Terms'), 'hms', array('type'=>'term', 'op'=>'show_edit_terms'));
 
         /***********************
