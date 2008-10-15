9e4588bffe824c948bdbab862c12a5f77c4f3a62
dwest <westwolf24@gmail.com>
2008-10-15 19:38:32 +0000

    Added templates too ^^;;

---
+++ class/HMS_Term_Applications.php
@@ -0,0 +1,49 @@
+<?php
+/**
+  * Wrapper class for the hms_term_applications table.
+  * TODO: give this class/table a better name
+  *
+  * @author     Daniel West <dwest at tux dot appstate dot edu>
+  * @package    mod
+  * @subpackage hms
+  */
+class HMS_Term_Applications {
+    var $app_term;
+    var $term;
+    var $required;
+
+    function getPager()
+    {
+        PHPWS_Core::initCoreClass('DBPager.php');
+        $pager = &new DBPager('hms_term_applications', 'HMS_Term_Applications');
+        $pager->setModule('hms');
+        $pager->addRowTags('get_row_tags');
+        $pager->setTemplate('admin/application_terms_list.tpl');
+        $pager->setEmptyMessage('No rows returned.');
+        $pager->setOrder('app_term', 'desc');
+
+        return $pager->get();
+    }
+
+    function get_row_tags()
+    {
+        $tpl['REQUIRED']    = ($this->required == 1 ? 'yes' : 'no'); 
+        $tpl['DELETE']      = '<a href=index.php?module=hms&type=term&op=show_term_association&delete='.$this->app_term.'>Delete</a>';
+
+        return $tpl;
+    }
+
+    function remove($app_term)
+    {
+        $db = &new PHPWS_DB('hms_term_applications');
+        $db->addWhere('app_term', $app_term);
+        $result = $db->delete();
+
+        if(PHPWS_Error::logIfError($result)){
+            return false;
+        }
+
+        return true;
+    }
+}
+?>
---
+++ templates/admin/application_terms_list.tpl
@@ -0,0 +1,11 @@
+<!-- BEGIN empty -->
+{EMPTY_MESSAGE}
+<!-- END empty -->
+<!-- BEGIN listrows -->
+<tr>
+    <td>{APP_TERM}</td>
+    <td>{TERM}</td>
+    <td>{REQUIRED}</td>
+    <td>{DELETE}</td>
+</tr>
+<!-- END listrows -->
