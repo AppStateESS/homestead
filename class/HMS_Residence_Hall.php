3caee1f015a045a08d0bde27440f1f4cffbb342b
jbooker <jeremy.booker@gmail.com>
2008-10-30 22:12:25 +0000

    + Renamed HMS_Residence_Hall::count_lottery_used_rooms() to count_lottery_full_rooms(); + Added a count_lottery_used_rooms() which counts a room if it is used at all.

--- class/HMS_Residence_Hall.php
+++ class/HMS_Residence_Hall.php
@@ -581,6 +581,22 @@ class HMS_Residence_Hall extends HMS_Item
 
     function count_lottery_used_rooms()
     {
+
+        $query = "SELECT count(hms_room.*) FROM hms_room 
+                       JOIN hms_floor ON hms_room.floor_id = hms_floor.id
+                       JOIN hms_residence_hall ON hms_floor.residence_hall_id = hms_residence_hall.id
+                       AND hms_residence_hall.id = {$this->id} AND
+                       hms_room.id IN (SELECT DISTINCT hms_room.id FROM hms_room
+                       JOIN hms_bed ON hms_bed.room_id = hms_room.id
+                       JOIN hms_floor ON hms_room.floor_id = hms_floor.id
+                       JOIN hms_residence_hall ON hms_floor.residence_hall_id = hms_residence_hall.id
+                       WHERE (hms_bed.id IN (SELECT bed_id FROM hms_lottery_reservation WHERE term = {$this->term} AND expires_on > $now)
+                       OR hms_bed.id IN (SELECT bed_id FROM hms_assignment WHERE term = {$this->term} and lottery = 1))
+                       AND hms_residence_hall.id = {$this->id})";
+    }
+
+    function count_lottery_full_rooms()
+    {
         $now = mktime();
 
         # Get the number of rooms in this hall which have every bed either assigned or reserved through the lottery.
