f8736ee602c40e862860a5cc05b67f7a1cc3ee69
jbooker <jeremy.booker@gmail.com>
2008-11-05 15:09:56 +0000

    + Improving the look of the login screen, more work yet to be done

--- class/HMS_Login.php
+++ class/HMS_Login.php
@@ -6,75 +6,56 @@ class HMS_Login
 {
     function display_login_screen($error = NULL)
     {
-        PHPWS_Core::initCoreClass('Form.php');
-        $form = &new PHPWS_Form;
-
-        $form->addText('asu_username');
-        $form->addPassword('password');
-
-        $form->addHidden('module', 'hms');
-        $form->addHidden('type', 'hms');
-        $form->addHidden('op', 'login');
-        $form->addSubmit('submit', _('Login'));
-
-        $tpl = $form->getTemplate();
-        $welcome  = "Welcome to the Housing Management System.<br /><br />";
-        $welcome .= "There are multiple parts to this process. These are:<br />";
-        $welcome .= " - Logging in<br />";
-        $welcome .= " - Agreeing to the Housing License Contract<br />";
-        $welcome .= " - Completing a Housing Application<br />";
-        $welcome .= " - Completing the Residential Learning Community Application if you wish to participate in a RLC<br />";
-        $welcome .= " - Completing the *OPTIONAL* student profile<br /><br />";
-        $welcome .= "Please note that once you complete the Housing Application you do not have to fill out anything else provided at this website.<br /><br />";
-      
-        $welcome .= "<br /><br />";
-        $welcome .= "<b>If you are experiencing problems please read <a href=\"./index.php?module=webpage&id=1\" target=\"_blank\">this page</a>.</b>";
-        $welcome .= "<br /><br />";
-
-        $values = array('ADDITIONAL'=>'The Housing Management System will <strong>not</strong> work without cookies.  Please read about <a href="http://www.google.com/cookies.html" target="_blank">how to enable cookies</a>.');
+        $values = array('ADDITIONAL'=>'The Housing Management System will <strong>not</strong> work without having your web browser\'s cookie features enabled.  Please read about <a href="http://www.google.com/cookies.html" target="_blank">how to enable cookies</a>.');
         $tpl['COOKIE_WARNING'] = Layout::getJavascript('cookietest', $values);
-        $tpl['WELCOME'] = $welcome;
-        $tpl['ERROR']   = $error;
-        $final = PHPWS_Template::process($tpl, 'hms', 'misc/login.tpl');
 
-        Layout::add($final);
+        # If the user has cookies enabled (and therefore is not being shown the cookie warning message...
+        if(is_null($tpl['COOKIE_WARNING'])){
+            $tpl['login'] = ''; // a dummy tag to make the actual login content show
+        }
+
+        Layout::add(PHPWS_Template::process($tpl, 'hms', 'misc/login.tpl'));
     }
 
     function login_user()
     {
+        /*
         if(!file_exists(AXP_LOCATION)) {
             return "A critical error has occurred in the Housing Management System.  Please tell Electronic Student Services that their AXP driver is missing.";
         }
-
+        */
+        /*
         require_once(PHPWS_SOURCE_DIR . AXP_LOCATION);
 
         if($type = axp_authorize($_REQUEST['asu_username'], $_REQUEST['password'])) {
             return HMS_Login::student_login();
         } else {
+        */
             if(Current_User::loginUser($_REQUEST['asu_username'], $_REQUEST['password'])){
                 Current_User::getLogin();
                 return ADMIN;
             } else {
                 return BADTUPLE;
             }
-        }
+            //        }
         
     }
 
-    function student_login()
+    function student_login($username)
     {
         PHPWS_Core::initModClass('hms','HMS_Deadlines.php');
         PHPWS_Core::initModClass('hms','HMS_SOAP.php');
         $deadlines = HMS_Deadlines::get_deadlines();
 
         /* Don't destroy our admin session if an admin is logging in as a user */
+        /*
         if( !Current_User::isLogged() ) {
             require_once(PHPWS_SOURCE_DIR . '/mod/hms/inc/accounts.php');
             Current_User::loginUser(HMS_STUDENT_USER, HMS_STUDENT_PASS);
             Current_User::getLogin();
         }
-
-        $username = strtolower(trim($_REQUEST['asu_username']));
+        */
+        //        $username = strtolower(trim($_REQUEST['asu_username']));
 
         # Log the student's login in their activity log
         PHPWS_Core::initModClass('hms','HMS_Activity_Log.php');
@@ -83,6 +64,7 @@ class HMS_Login
         # Setup the session variable
         $_SESSION['asu_username'] = $username;
         $_SESSION['application_term'] = HMS_SOAP::get_application_term($username);
+        $_REQUEST['op'] = 'main';
         return STUDENT;
     }
 
--- templates/misc/login.tpl
+++ templates/misc/login.tpl
@@ -2,24 +2,16 @@
 <div class="hms">
   <div class="box">
     <div class="box-title"> 
-    <h1>LOGIN</h1><br />
-    <!-- BEGIN error_msg -->
-    <span class="error">{ERROR}</span><br/>
-    <!-- END error_msg -->
-  </div>
+    <h1>Welcome to the Appalachian State University Housing Management System</h1><br />
+    </div>
     <div class="box-content">
+    <!-- BEGIN cookie_warning -->
     {COOKIE_WARNING}
-    {WELCOME}<br />
-        <table>
-            <tr>
-                <th>Username: &nbsp;&nbsp;</th><td>{ASU_USERNAME}</td>
-            </tr>
-            <tr>
-                <th>Password: </th><td>{PASSWORD}</td>
-            </tr>
-        </table>
-        <br />
-        {SUBMIT}
+    <!-- END cookie_warning -->
+    <!-- BEGIN login -->
+    {login}
+    Please <a href="/login">login</a> to continue. [ <a href="#">more information</a> ]
+    <!-- END login -->
     </div>
   </div>
 </div>
