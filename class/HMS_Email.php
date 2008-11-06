418b8296af839a37edb8ad4082e65cd0de9b75fb
dwest <westwolf24@gmail.com>
2008-11-06 18:13:00 +0000

    Removed the update email function/template since it isn't really being used.

--- class/HMS_Email.php
+++ class/HMS_Email.php
@@ -236,18 +236,5 @@ class HMS_Email{
         HMS_Email::send_template_message($to . '@appstate.edu', '[Testing] Housing Assignment Notice!', 'email/assignment_notice.tpl', $tpl);
     }
 
-    /*
-    function send_assignment_update_email($to, $name, $location, $roommates){
-        $tpl = array();
-
-        $tpl['NAME']        = $name;
-        $tpl['HALL']        = $hall;
-        $tpl['ROOM']        = $room;
-        $tpl['YEAR']        = $year;
-        $tpl['roommates']   = $roommates;
-
-        HMS_Email::send_template_message($to . '@appstate.edu', '[Testing] Housing Assignment Notice!', 'email/assignment_update_notice.tpl', $tpl);
-    }
-    */
 } // End HMS_Email class
 ?>
