811ee3d34a1df4d78a35cbf95f8b5f02cf8e8d17
jbooker <jeremy.booker@gmail.com>
2009-01-23 18:37:32 +0000

    Bumping version number to 0.3.10 and added permissions update to update.php

--- boost/boost.php
+++ boost/boost.php
@@ -1,6 +1,6 @@
 <?php
     $proper_name  = 'Housing Management System';
-    $version      = '0.3.9';
+    $version      = '0.3.10';
     $import_sql   = TRUE;
     $image_dir    = TRUE;
     $file_dir     = FALSE;
--- boost/update.php
+++ boost/update.php
@@ -1030,6 +1030,9 @@ function hms_update(&$content, $currentVersion)
             if(PEAR::isError($result)){
                 return $result;
             }
+        case version_compare($currentVersion, '0.3.10', '<'):
+            PHPWS_Core::initModClass('users', 'Permission.php');
+            Users_Permission::registerPermissions('hms', $content);
     }
 
     return TRUE;
