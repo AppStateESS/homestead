e5756d4990efb8f562d35bfe8df10e33e5a021e9
jbooker <jeremy.booker@gmail.com>
2009-07-01 14:11:21 +0000

    Upped version number to 0.3.26

--- boost/boost.php
+++ boost/boost.php
@@ -1,6 +1,6 @@
 <?php
     $proper_name  = 'Housing Management System';
-    $version      = '0.3.25';
+    $version      = '0.3.26';
     $import_sql   = TRUE;
     $image_dir    = TRUE;
     $file_dir     = FALSE;
