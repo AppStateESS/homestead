073bf54241dd07706db04ee1edbaf8a65b227641
jbooker <jeremy.booker@gmail.com>
2009-11-20 21:10:12 +0000

    Fixed missing closing brackets in jquery head.js. Was cuasing chrome to flip it's lid.

--- javascript/jquery_ui/head.js
+++ javascript/jquery_ui/head.js
@@ -1,4 +1,4 @@
 <script type="text/javascript" src="javascript/modules/hms/jquery_ui/jquery.ui.all.js"></script>
-<script type="text/javascript" src="javascript/modules/hms/jquery_ui/ui/ui.dialog.js"></script
-<script type="text/javascript" src="javascript/modules/hms/jquery_ui/ui/ui.resizable.js"></script
-<script type="text/javascript" src="javascript/modules/hms/jquery_ui/ui/ui.draggable.js"></script
+<script type="text/javascript" src="javascript/modules/hms/jquery_ui/ui/ui.dialog.js"></script>
+<script type="text/javascript" src="javascript/modules/hms/jquery_ui/ui/ui.resizable.js"></script>
+<script type="text/javascript" src="javascript/modules/hms/jquery_ui/ui/ui.draggable.js"></script>
