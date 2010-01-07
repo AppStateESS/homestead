1952d16bca810759d1d3a685ceac7c1f964e9e0f
jbooker <jeremy.booker@gmail.com>
2010-01-07 14:44:38 +0000

    Syntax error fix in 0_4_2.sql update.

--- boost/0_4_2.sql
+++ boost/0_4_2.sql
@@ -1,6 +1,6 @@
 ALTER TABLE hms_application_feature RENAME "startdate" TO start_date;
 ALTER TABLE hms_application_feature RENAME "enddate" TO end_date;
 ALTER TABLE hms_application_feature ADD COLUMN enabled smallint NOT NULL DEFAULT 0;
-ALTER TABLE hms_application_feature ADD PRIMARY KEY id;
+ALTER TABLE hms_application_feature ADD PRIMARY KEY (id);
 ALTER TABLE hms_term ADD COLUMN pdf_terms VARCHAR(255);
 ALTER TABLE hms_term ADD COLUMN txt_terms VARCHAR(255);
