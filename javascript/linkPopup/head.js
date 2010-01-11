6177551c9d3eac9d5c475c68957d7035fa01120f
jtickle <devnull@localhost>
2010-01-11 15:56:48 +0000

    Popup now works.

--- javascript/linkPopup/head.js
+++ javascript/linkPopup/head.js
@@ -1,6 +1,7 @@
 <script type="text/javascript" src="javascript/modules/hms/linkPopup/jquery.linkPopup.js"></script>
 <script type="text/javascript">
 $(document).ready(function() {
+	$("#linkPopupDialog").dialog({autoOpen: false, modal: true, width: 425, resizable: false, position: 'top'});
 	$('{LINK_SELECT}').linkPopup();
 });
 </script>
