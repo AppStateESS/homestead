da7a471038561fa4e5cf3c1f36207a3199be3831
jbooker <jeremy.booker@gmail.com>
2010-01-11 20:11:00 +0000

    Added permission check to admin maintenece menu command.

--- class/command/ShowAdminMaintenanceMenuCommand.php
+++ class/command/ShowAdminMaintenanceMenuCommand.php
@@ -9,6 +9,12 @@ class ShowAdminMaintenanceMenuCommand extends Command {
     function execute(CommandContext $context)
     {
         PHPWS_Core::initModClass('hms', 'AdminMaintenanceMenuView.php');
+
+        if(!Current_User::isUnrestricted('hms')){
+            PHPWS_Core::initModClass('hms', 'exception/PermissionException.php');
+            throw new PermissionException('You do not have permission to view the admin maintenance menu.');
+        }
+
         $adminMenu = new AdminMaintenanceMenuView();
 
         $context->setContent($adminMenu->show());
