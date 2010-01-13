540ba2387a1751a46c36843c41f86496c99507cc
jbooker <jeremy.booker@gmail.com>
2010-01-13 14:51:51 +0000

    Fixed minor bug in LotteryAdminCreateAppCommand (set special_interest to null instead of 'none'). Also, added exception handling for SOAP lookup.

--- class/command/LotteryAdminCreateAppCommand.php
+++ class/command/LotteryAdminCreateAppCommand.php
@@ -17,17 +17,34 @@ class LotteryAdminCreateAppCommand extends Command {
 
         PHPWS_Core::initModClass('hms', 'LotteryApplication.php');
         PHPWS_Core::initModClass('hms', 'StudentFactory.php');
+        
+        $viewCmd = CommandFactory::getCommand('ShowLotteryAdminEntry');
 
         $term = Term::getSelectedTerm();
-        $student = StudentFactory::getStudentByUsername($context->get('asu_username'), $term);
+        
+        $username = $context->get('asu_username');
+        if(!isset($username) || empty($username)){
+            NQ::simple('hms', HMS_NOTIFICATION_ERROR, 'You must enter a valid user name.');
+            $viewCmd->redirect();
+        }
+        
+        try{
+            $student = StudentFactory::getStudentByUsername($context->get('asu_username'), $term);
+        }catch(StudentNotFoundException $e){
+            NQ::simple('hms', HMS_NOTIFICATION_ERROR, 'Inavlid user name. No student with that user name could be found.');
+            $viewCmd->redirect();
+        }
 
         $physicalDisability = $context->get('physical_disability');
         $psychDisability    = $context->get('psych_disability');
         $genderNeed         = $context->get('gender_need');
         $medicalNeed        = $context->get('medical_need');
         $specialInterest    = $context->get('special_interest');
-        $viewCmd = CommandFactory::getCommand('ShowLotteryAdminEntry');
-
+        
+        if($specialInterest == 'none'){
+            $specialInterest = NULL;
+        }
+        
         $application = new LotteryApplication(0, $term, $student->getBannerId(), $student->getUsername(), $student->getGender(), $student->getType(), $student->getApplicationTerm(), null, BANNER_MEAL_STD, $physicalDisability, $psychDisability, $genderNeed, $medicalNeed, NULL, $specialInterest);
 
         try{
