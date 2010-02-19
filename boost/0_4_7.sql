c6f50787cf92c06523231cafff7ac0754367ad81
jbooker <jeremy.booker@gmail.com>
2010-02-19 21:04:06 +0000

    Added SQL to update script to delete previous roommate profiles.

--- boost/0_4_7.sql
+++ boost/0_4_7.sql
@@ -1,3 +1,4 @@
+delete from hms_student_profiles;
 alter table hms_student_profiles add column term integer references hms_term(term);
 update hms_student_profiles set term = 200940;
 alter table hms_student_profiles alter column term SET NOT NULL;
