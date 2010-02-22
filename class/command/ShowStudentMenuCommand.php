603e612d50252fc5aa0a136318df79da7094f584
jbooker <jeremy.booker@gmail.com>
2010-02-22 19:47:12 +0000

    Fixed reference to $_SESSION['asu_username'] in ShowStudentMenuCommand. Only a problem for students who don't have Banner data yet.

--- class/command/ShowStudentMenuCommand.php
+++ class/command/ShowStudentMenuCommand.php
@@ -41,7 +41,7 @@ class ShowStudentMenuCommand extends Command {
         is_null($dob))
         {
             # TODO: HMS_Mail here
-            PHPWS_Error::log('Initial banner lookup failed', 'hms', 'show_welcome_screen', "username: {$_SESSION['asu_username']}");
+            PHPWS_Error::log('Initial banner lookup failed', 'hms', 'show_welcome_screen', "username: " . UserStatus::getUsername());
             $badDataCmd = CommandFactory::getCommand('ShowBadBannerData');
             $badDataCmd->redirect();
         }
