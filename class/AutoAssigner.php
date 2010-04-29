ac6ec371c5f500d0c64e17db21250eaae25108bb
jbooker <jeremy.booker@gmail.com>
2010-04-29 13:28:04 -0400

    More work on auto-assigner.

--- class/AutoAssigner.php
+++ class/AutoAssigner.php
@@ -16,6 +16,7 @@ class AutoAssigner {
     // Student state tracking
     private $usersToAssign      = array(); // Simple array of user names to assign
     private $assignedStatus     = array(); // List of all students to be assigned, and whether they've been assigned or not
+
     // Room state tracking - will pop from room arrays as rooms are used
     private $freeFemaleRooms    = array();
     private $freeMaleRooms      = array();
@@ -30,20 +31,39 @@ class AutoAssigner {
         $this->term     = $term;
         $this->debug    = $debug;
         
-        // Get all the free rooms
-        $this->freeFemaleRooms = HMS_Room::getAlLFreeRooms($this->term, FEMALE);
-        $this->freeFemaleRooms = HMS_Room::getAlLFreeRooms($this->term, MALE);
-        
         // Get everyone who needs to be assigned
     }
     
     public function autoAssign()
     {
+        // Get a list of all the single-gender rooms for each gender
+        
+        // Assign all the females who want single gender rooms
+        
+        // Assign all the males who want single gender rooms
+        
+        // Re-compile the list of free rooms for each gender, regardless of single or co-ed
+        // This ensures we use all available rooms, even if we end up assigning a co-ed
+        // preference person to a single-gender hall
+        $this->freeFemaleRooms  = HMS_Room::getAlLFreeRooms($this->term, FEMALE);
+        $this->freeMaleRooms    = HMS_Room::getAlLFreeRooms($this->term, MALE);
+        
+        // Re-compile the list of people to be assigned
+        // Doing this after the single-gender assignments
+        // ensures that we'll catch students who couldn't be assigned
+        // if we ran out of single-gender rooms
+        $this->usersToAssign = HousingApplication::getUnassignedApplicants($this->term);
+        
+        // Assign everyone (males and females at the same time, to ensure we don't
+        // run out of rooms with a bunch of one gender left
         
     }
     
-    public function assignForGender($gender)
-    {
+    /**
+     * Takes a gender, and a gender preference (single gender vs. co-ed)
+     * and returns an array of free rooms of that type 
+     */
+    private function getFreeRooms($gender, $genderPreference = NULL){
         
     }
 }
