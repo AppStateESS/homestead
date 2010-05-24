90fe5dc70d886349b1fd02fad4b865ec5394fd0f
jbooker <jeremy.booker@gmail.com>
2010-05-24 13:59:00 -0400

    Hopefully fixed RLC application bug. (Refs #568)

--- class/RlcApplicationMenuView.php
+++ class/RlcApplicationMenuView.php
@@ -1,14 +1,14 @@
 <?php
 
 class RlcApplicationMenuView extends View {
-    
+
     private $term;
     private $student;
     private $startDate;
     private $editDate;
     private $endDate;
     private $application;
-    
+
     public function __construct($term, Student $student, $startDate, $editDate, $endDate, HMS_RLC_Application $application = NULL)
     {
         $this->term         = $term;
@@ -18,7 +18,7 @@ class RlcApplicationMenuView extends View {
         $this->endDate      = $endDate;
         $this->application  = $application;
     }
-    
+
     public function show()
     {
         $tpl = array();
@@ -27,23 +27,24 @@ class RlcApplicationMenuView extends View {
             $viewCmd = CommandFactory::getCommand('ShowRlcApplicationReView');
             $viewCmd->setAppId($this->application->getId());
             $tpl['VIEW_APP'] = $viewCmd->getLink('view your application');
-            
+
             if(time() < $this->editDate){
                 $newCmd = CommandFactory::getCommand('ShowRlcApplicationView');
                 $newCmd->setTerm($this->term);
                 $tpl['NEW_APP'] = $newCmd->getLink('submit a new application');
             }
         }else if(time() < $this->startDate){
-            $tpl['BEGIN_DEADLINE'] = HMS_Util::getFriendlyDate($this->startDate); 
+            $tpl['BEGIN_DEADLINE'] = HMS_Util::getFriendlyDate($this->startDate);
         }else if (time() > $this->endDate){
             $tpl['END_DEADLINE'] = HMS_Util::getFriendlyDate($this->endDate);
         }else{
             $applyCmd = CommandFactory::getCommand('ShowRlcApplicationView');
+            $applyCmd->setTerm($this->term);
             $tpl['APP_NOW'] = $applyCmd->getLink('Apply for a Residential Learning Community now.');
         }
 
         Layout::addPageTitle("RLC Application Menu");
-        
+
         return PHPWS_Template::process($tpl, 'hms', 'student/menuBlocks/RlcApplicationMenuBlock.tpl');
     }
 }
