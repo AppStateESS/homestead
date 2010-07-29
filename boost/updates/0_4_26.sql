9dbff360fbfa4f351992ee8a8fd050607cdfb9e7
jbooker <jeremy.booker@gmail.com>
2010-07-29 16:44:01 -0400

    Updated 0_4_26 sql update script to add default roles, permissions.

--- boost/updates/0_4_26.sql
+++ boost/updates/0_4_26.sql
@@ -4,6 +4,13 @@ CREATE TABLE hms_role (
     PRIMARY KEY(id)
 );
 
+INSERT INTO hms_role VALUES (1, 'RDs');
+INSERT INTO hms_role VALUES (2, 'RLCs');
+INSERT INTO hms_role VALUES (3, 'Assoc Director');
+
+CREATE SEQUENCE hms_role_seq;
+SELECT setval('hms_role_seq', (SELECT max(id) FROM hms_role));
+
 CREATE TABLE hms_permission (
     id                  INTEGER                 NOT NULL,
     name                VARCHAR(32)             NOT NULL,
@@ -11,11 +18,21 @@ CREATE TABLE hms_permission (
     PRIMARY KEY(id)
 );
 
+INSERT INTO hms_permission VALUES (1, 'email', 'send emails');
+CREATE SEQUENCE hms_permission_seq;
+SELECT setval('hms_permission_seq', (SELECT max(id) FROM hms_permission));
+
+CREATE SEQUENCE hms_permission_seq;
+
 CREATE TABLE hms_role_perm (
     role                INTEGER NOT NULL REFERENCES hms_role(id),
     permission          INTEGER NOT NULL REFERENCES hms_permission(id),
     PRIMARY KEY(role, permission)
 );
+
+INSERT INTO hms_role_perm VALUES (1,1);
+INSERT INTO hms_role_perm VALUES (2,1);
+INSERT INTO hms_role_perm VALUES (3,1);
 
 CREATE TABLE hms_user_role (
     id                  INTEGER NOT NULL,
@@ -26,3 +43,4 @@ CREATE TABLE hms_user_role (
     UNIQUE (user_id, role, class, instance),
     PRIMARY KEY(id)
 );
+
