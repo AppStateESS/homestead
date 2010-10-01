a632186d2f830008a80bafd7ee5fd9df8f73aa8e
jbooker <jeremy.booker@gmail.com>
2010-10-01 16:03:16 -0400

    Fixed display of term on student main menu.
    Refs #619

--- class/ReturningMainMenuView.php
+++ class/ReturningMainMenuView.php
@@ -22,14 +22,38 @@ class ReturningMainMenuView extends View {
 	{
 		$tpl = array();
 
+        $termList = array();
+
+        // Current term
 		$currTerm = Term::getCurrentTerm();
-		$springTerm = Term::getNextTerm($this->lotteryTerm);
-		$summerTerm1 = Term::getNextTerm(Term::getCurrentTerm());
+        $termList[] = $currTerm; // Always add the current term
+
+        // Find the next two summer terms (could be next year if Fall 
+        // is the current term, could be this year if Spring is current term)
+        $summerTerm1 = $currTerm;
+        while(Term::getTermSem($summerTerm1) != TERM_SUMMER1){
+            $summerTerm1 = Term::getNextTerm($summerTerm1);
+        }
 		$summerTerm2 = Term::getNextTerm($summerTerm1);
 
-		$terms = array($currTerm, $this->lotteryTerm, $summerTerm1, $summerTerm2);
+        $currSem = Term::getTermSem($currTerm);
+        if($currSem == TERM_SUMMER1){
+            // If the current term is Summer 1, then we've already added it above,
+            // so just add summer 2
+            $termList[] = Term::getNextTerm($currTerm);
+        }else if($currSem != TERM_SUMMER2){
+            // Add both of the next summer terms then
+            $termList[] = $summerTerm1;
+            $termList[] = $summerTerm2;
+        }
 
-		foreach($terms as $t){
+
+        // Re-application term
+        if($this->lotteryTerm > $currTerm){ // If the lottery term is in the future
+            $termList[] = $this->lotteryTerm;
+        }
+
+		foreach($termList as $t){
 			$termBlock = new StudentMenuTermBlock($this->student, $t);
 			$tpl['TERMBLOCK'][] = array('TERMBLOCK_CONTENT'=>$termBlock->show());
 		}
