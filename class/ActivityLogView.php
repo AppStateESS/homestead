7c89de82561b74a6392a45b2ced453a1cba988da
jbooker <jeremy.booker@gmail.com>
2010-10-05 12:39:49 -0400

    Fixed jquery_ui path.

--- class/ActivityLogView.php
+++ class/ActivityLogView.php
@@ -41,7 +41,7 @@ class ActivityLogView extends View {
 
         Layout::addPageTitle("Activity Log");
 
-        javascript('/jquery_ui');
+        javascript('jquery_ui');
         javascript('/modules/hms/note', array('LINK'=>'activity-log-note'));
 
 		return PHPWS_Template::Process($tags, 'hms', 'admin/activity_log_box.tpl');
