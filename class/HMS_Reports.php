3c453676919516261e010440eb7f3f3242c7505e
bostrt <devnull@localhost>
2010-11-29 14:18:20 -0500

    Added lottery bed count for each residence hall.

--- class/HMS_Reports.php
+++ class/HMS_Reports.php
@@ -1587,6 +1587,7 @@ class HMS_Reports{
      */
     public static function reappAvailability()
     {
+        // Available rooms in each residence hall.
         $db = new PHPWS_DB('hms_bed');
         $db->addJoin('LEFT', 'hms_bed', 'hms_room', 'room_id', 'id');
         $db->addJoin('LEFT', 'hms_room', 'hms_floor', 'floor_id', 'id');
@@ -1599,28 +1600,44 @@ class HMS_Reports{
         $db->addWhere('hms_room.is_online', 1);
         $term = Term::getSelectedTerm();
         $db->addWhere('hms_bed.term', $term);
-        $db->addWhere('hms_room.term', $term);
-        $db->addWhere('hms_floor.term', $term);
-        $db->addWhere('hms_residence_hall.term', $term);
         $db->addColumn('hms_room.room_number');
         $db->addColumn('hms_bed.bed_letter', null, null, True);
         $db->addColumn('hms_residence_hall.hall_name');
         $db->addGroupBy('hms_residence_hall.hall_name');
         $db->addGroupBy('hms_room.room_number');
         $db->addOrder('hms_residence_hall.hall_name');
-        //$db->setTestMode();
-        $result = $db->select();
+        $availRooms = $db->select();
+        
+        // Allocated beds for lottery.
+        $db = new PHPWS_DB('hms_bed');
+        $db->addJoin('LEFT' , 'hms_bed', 'hms_room', 'room_id', 'id');
+        $db->addJoin('LEFT' , 'hms_room', 'hms_floor', 'floor_id', 'id');
+        $db->addJoin('LEFT' , 'hms_floor', 'hms_residence_hall', 'residence_hall_id', 'id');
+        $db->addJoin('RIGHT', 'hms_bed', 'hms_lottery_reservation', 'id', 'bed_id');
+        $db->addWhere('hms_lottery_reservation.term', $term);
+        $db->addColumn('hms_residence_hall.hall_name');
+        $db->addColumn('hms_bed.id', null, null, True);
+        $db->addGroupBy('hms_residence_hall.hall_name');
+        $db->setIndexBy('hall_name');
+        $lotteryBeds = $db->select();
 
         $tpl = new PHPWS_Template('hms');
         $tpl->setFile('admin/reports/reapp_availability.tpl');
 
+        //
+        // "The parent row must be parsed after the child rows."
+
         // Preload currHall with first residence hall name
-        $currHall = $result[0]['hall_name'];
-        foreach($result as $row){
+        $currHall = $availRooms[0]['hall_name'];
+        foreach($availRooms as $row){
             // Change halls, create new block.
             if($currHall != $row['hall_name'] || $currHall == null){
                 $tpl->setCurrentBlock('halls');
-                $tpl->setData(array('HALL_NAME' => $currHall));
+                // Get allocated beds for the residence hall.
+                $lottCount = isset($lotteryBeds[$currHall]['count']) ? $lotteryBeds[$currHall]['count'] : 0;
+                $tpl->setData(array('HALL_NAME' => $currHall,
+                                    'LOTTERY_BEDS' => $lottCount));
+
                 $tpl->parseCurrentBlock();
                 $currHall = $row['hall_name'];
             }
--- templates/admin/reports/reapp_availability.tpl
+++ templates/admin/reports/reapp_availability.tpl
@@ -1,10 +1,11 @@
+<h1>Re-application Availability Report</h1>
 <table cellpadding="5">
   <tr>
     <!-- BEGIN halls -->
     <td>
-      <table width="200" border="1">
+      <table width="250" border="1">
         <tr>
-          <th height="75" colspan="2"><h3>{HALL_NAME}</h3></th>
+          <th height="75" colspan="2"><h3>{HALL_NAME}</h3><h4>Lottery Beds: {LOTTERY_BEDS}</h4></th>
         <tr><th>Room Number</th><th>Bed Count</th></tr>
         <!-- BEGIN rooms -->
         <tr><td>{ROOM_NUM}</td><td>{BED_COUNT}</td></tr>
