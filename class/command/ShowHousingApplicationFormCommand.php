4e9147502a9a113851904695040d39adc11d18fc
jbooker <jeremy.booker@gmail.com>
2011-02-25 10:34:03 -0500

    Changed sanity check and improved the error message.

--- class/command/ShowHousingApplicationFormCommand.php
+++ class/command/ShowHousingApplicationFormCommand.php
@@ -3,69 +3,69 @@
 PHPWS_Core::initModClass('hms', 'Command.php');
 
 class ShowHousingApplicationFormCommand extends Command {
-	
+
 	private $term;
 	private $agreedToTerms;
 	private $vars;
-	
+
 	public function setTerm($term){
 		$this->term = $term;
 	}
-	
+
 	public function setAgreedToTerms($agreed){
 		$this->agreedToTerms = $agreed;
 	}
-	
+
 	public function setVars($vars)
 	{
 		$this->vars = $vars;
 	}
-	
+
 	public function getRequestVars()
 	{
 		$vars = $this->vars;
-		
+
 		$vars['action'] = 'ShowHousingApplicationForm';
 		unset($vars['module']);
-		
-		
+
+
 		if(isset($this->term)){
 			$vars['term'] = $this->term;
 		}
-		
+
 		if(isset($this->agreedToTerms)){
 			$vars['agreedToTerms'] = $this->agreedToTerms;
 		}
-		
+
 		return $vars;
 	}
-	
+
 	public function execute(CommandContext $context)
 	{
 		PHPWS_Core::initModClass('hms', 'StudentFactory.php');
 		PHPWS_Core::initModClass('hms', 'HousingApplication.php');
 		PHPWS_Core::initModClass('hms', 'HousingApplicationFactory.php');
 		PHPWS_Core::initModClass('hms', 'HousingApplicationFormView.php');
-		
+
 		# Make sure we have a valid term
 		$term = $context->get('term');
-		
+
 		if(is_null($term) || !isset($term)){
 			throw new InvalidArgumentException('Missing term.');
 		}
-		
+
 		$student = StudentFactory::getStudentByUsername(UserStatus::getUsername(), $term);
 
-        // Right now this form is only used for freshmen, so make sure the student is one
-        if($student->getType() != TYPE_FRESHMEN){
-            NQ::simple('hms', HMS_NOTIFICATION_ERROR, 'Cannot fill out a freshman application as a non-freshman!');
+        // Sanity check to prevent 'continuing' students from completing this form
+        if($student->getType() == TYPE_CONTINUING || $student->getType() == TYPE_READMIT || $student->getApplicationTerm() <= Term::getCurrentTerm()){
+            NQ::simple('hms', HMS_NOTIFICATION_ERROR, 'Sorry, as a non-freshman student, you cannot complete this application (which is for freshmen only). If you think you have reached this message in error, please contact University Housing using the form below.');
             $cmd = CommandFactory::getCommand('ShowContactForm');
             $cmd->redirect();
         }
-		
+
 		# Make sure the student agreed to the terms, if not, send them back to the terms & agreement command
 		$agreedToTerms = $context->get('agreedToTerms');
-		
+
 		# If they haven't agreed, redirect to the agreement
 		if(is_null($agreedToTerms) || !isset($agreedToTerms) || $agreedToTerms != 1){
 			$agreementCmd = CommandFactory::getCommand('ShowTermsAgreement');
@@ -73,12 +73,12 @@ class ShowHousingApplicationFormCommand extends Command {
 			$agreementCmd->setAgreedCommand(CommandFactory::getCommand('ShowHousingApplicationForm'));
 			$agreementCmd->redirect();
 		}
-		
+
 		//TODO add side thingie
-		
+
 		# Check to see if the user has an existing application for the term in question
 		$existingApplication = HousingApplication::getApplicationByUser($student->getUsername(), $term);
-		
+
 		# Check for an in-progress application on the context, ignore any exceptions (in case there isn't one)
 		try {
 			$contextApplication = HousingApplicationFactory::getApplicationFromContext($context, $term, $student);
@@ -86,9 +86,9 @@ class ShowHousingApplicationFormCommand extends Command {
 			// ignored
 			$contextApplication = NULL;
 		}
-		
+
 		$appView = new HousingApplicationFormView($student, $term, $existingApplication, $contextApplication);
-		
+
 		$context->setContent($appView->show());
 	}
 }
