d88381b7ecb9a444f56096ddda90a57f8e6d51c0
jbooker <jeremy.booker@gmail.com>
2011-05-09 10:21:34 -0400

    Dropped not null contstraint from 'state' field on hms_student_address_cache table.
    Refs #706

--- boost/updates/0_4_43.sql
+++ boost/updates/0_4_43.sql
@@ -1,3 +1,5 @@
 drop table hms_pricing_tiers;
 drop sequence hms_pricing_tiers_seq ;
 alter table hms_room drop column pricing_tier;
+
+alter table hms_student_address_cache alter column state drop not null;
