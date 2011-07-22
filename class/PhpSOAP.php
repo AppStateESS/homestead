afb76e7aa26f30b0964dec05d757a82fd24302b7
jbooker <jeremy.booker@gmail.com>
2011-07-22 15:26:49 -0400

    Fixed a bunch of tab indention.

--- class/PhpSOAP.php
+++ class/PhpSOAP.php
@@ -4,240 +4,240 @@ PHPWS_Core::initModClass('hms', 'SOAP.php');
 
 class PhpSOAP extends SOAP
 {
-	private $client; // SOAP client object
+    private $client; // SOAP client object
 
-	protected function __construct()
-	{
-		parent::__construct();
-		ini_set('soap.wsdl_cache_enabled', 0);
-		$this->client = new SoapClient('file://' . PHPWS_SOURCE_DIR . 'mod/hms/inc/shs0001.wsdl', array('trace'=>true));
-	}
+    protected function __construct()
+    {
+        parent::__construct();
+        ini_set('soap.wsdl_cache_enabled', 0);
+        $this->client = new SoapClient('file://' . PHPWS_SOURCE_DIR . 'mod/hms/inc/shs0001.wsdl', array('trace'=>true));
+    }
 
-	public function getStudentInfo($username, $term)
-	{
-		// Sanity checking on the username
-		if(empty($username) || is_null($username) || !isset($username)){
-			throw new InvalidArgumentException('Bad username');
-		}
+    public function getStudentInfo($username, $term)
+    {
+        // Sanity checking on the username
+        if(empty($username) || is_null($username) || !isset($username)){
+            throw new InvalidArgumentException('Bad username');
+        }
 
-		// Sanity checking on the term
-		if(empty($term) || is_null($term) || !isset($term)){
-			throw new InvalidArgumentException('Bad term');
-		}
+        // Sanity checking on the term
+        if(empty($term) || is_null($term) || !isset($term)){
+            throw new InvalidArgumentException('Bad term');
+        }
 
-		$params = array('StudentID'=>$username, 'TermCode'=>$term);
+        $params = array('StudentID'=>$username, 'TermCode'=>$term);
 
-		try{
-			$response = $this->client->GetStudentProfile($params);
-		}catch(SoapFault $e){
-			PHPWS_Core::initModClass('hms', 'exception/SOAPException.php');
-			throw new SOAPException($e->getMessage(), $e->getCode(), 'getStudentInfo', $params);
-			return false;
-		}
+        try{
+            $response = $this->client->GetStudentProfile($params);
+        }catch(SoapFault $e){
+            PHPWS_Core::initModClass('hms', 'exception/SOAPException.php');
+            throw new SOAPException($e->getMessage(), $e->getCode(), 'getStudentInfo', $params);
+            return false;
+        }
 
-		SOAP::logSoap('getStudentInfo', 'success', $username, $term);
+        SOAP::logSoap('getStudentInfo', 'success', $username, $term);
 
-		return $response->profile;
-	}
+        return $response->profile;
+    }
 
-	public function getUsername($bannerId)
-	{
-		$params = array('BannerID'=>$bannerId);
+    public function getUsername($bannerId)
+    {
+        $params = array('BannerID'=>$bannerId);
 
-		try{
-			$response = $this->client->getUserName($params);
-		}catch(SoapFault $e){
-			PHPWS_Core::initModClass('hms', 'exception/SOAPException.php');
-			throw new SOAPException($e->getMessage(), $e->getCode(), 'getUsername', $params);
-			return false;
-		}
+        try{
+            $response = $this->client->getUserName($params);
+        }catch(SoapFault $e){
+            PHPWS_Core::initModClass('hms', 'exception/SOAPException.php');
+            throw new SOAPException($e->getMessage(), $e->getCode(), 'getUsername', $params);
+            return false;
+        }
 
-		if(!isset($response->GetUserNameResult)){
-			PHPWS_Core::initModClass('hms', 'exception/StudentNotFoundException.php');
-			throw new StudentNotFoundException("No matching student found with Banner ID: $bannerId.");
-			return false;
-		}
+        if(!isset($response->GetUserNameResult)){
+            PHPWS_Core::initModClass('hms', 'exception/StudentNotFoundException.php');
+            throw new StudentNotFoundException("No matching student found with Banner ID: $bannerId.");
+            return false;
+        }
 
-		SOAP::logSoap('getUsername', 'success', $bannerId);
+        SOAP::logSoap('getUsername', 'success', $bannerId);
 
-		return $response->GetUserNameResult;
-	}
+        return $response->GetUserNameResult;
+    }
 
-	public function isValidStudent($username, $term)
-	{
+    public function isValidStudent($username, $term)
+    {
 
-	}
+    }
 
-	public function reportApplicationReceived($username, $term)
-	{
-		// meal plan code and meal code don't matter here
-		$params = array(
+    public function reportApplicationReceived($username, $term)
+    {
+        // meal plan code and meal code don't matter here
+        $params = array(
                         'StudentID' => $username,
                         'TermCode'  => $term,
                         'PlanCode'  => 'HOME',
                         'MealCode'  => 1);
 
-		try{
-			$response = $this->client->CreateHousingApp($params);
-		}catch(SoapFault $e){
-			PHPWS_Core::initModClass('hms', 'exception/SOAPException.php');
-			throw new SOAPException($e->getMessage(), $e->getCode(), 'reportApplicationReceived', $params);
-			return false;
-		}
+        try{
+            $response = $this->client->CreateHousingApp($params);
+        }catch(SoapFault $e){
+            PHPWS_Core::initModClass('hms', 'exception/SOAPException.php');
+            throw new SOAPException($e->getMessage(), $e->getCode(), 'reportApplicationReceived', $params);
+            return false;
+        }
 
-		if($response->CreateHousingAppResult != "0"){
-			SOAP::logSoap('reportApplicationReceived', 'failed', $username, $term);
-			PHPWS_Core::initModClass('hms', 'exception/BannerException.php');
-			throw new BannerException('Error while reporting application to Banner.', $response->CreateHousingAppResult, 'reportApplicationReceived', $params);
-			return false;
-		}
+        if($response->CreateHousingAppResult != "0"){
+            SOAP::logSoap('reportApplicationReceived', 'failed', $username, $term);
+            PHPWS_Core::initModClass('hms', 'exception/BannerException.php');
+            throw new BannerException('Error while reporting application to Banner.', $response->CreateHousingAppResult, 'reportApplicationReceived', $params);
+            return false;
+        }
 
-		SOAP::logSoap('reportApplicationReceived', 'success', $username, $term);
-		return true;
-	}
+        SOAP::logSoap('reportApplicationReceived', 'success', $username, $term);
+        return true;
+    }
 
-	public function reportRoomAssignment($username, $term, $building, $room, $plan = 'HOME', $meal)
-	{
-		$params = array(
+    public function reportRoomAssignment($username, $term, $building, $room, $plan = 'HOME', $meal)
+    {
+        $params = array(
                         'StudentID'=>$username,
                         'TermCode'=>$term,
                         'BldgCode'=>$building,
                         'RoomCode'=>$room,
                         'PlanCode'=>$plan,
                         'MealCode'=>$meal);
-		try{
-			$response = $this->client->CreateRoomAssignment($params);
-		}catch(SoapFault $e){
-			PHPWS_Core::initModClass('hms', 'exception/SOAPException.php');
-			throw new SOAPException($e->getMessage(), $e->getCode(), 'reportRoomAssignment', $params);
-			return false;
-		}
+        try{
+            $response = $this->client->CreateRoomAssignment($params);
+        }catch(SoapFault $e){
+            PHPWS_Core::initModClass('hms', 'exception/SOAPException.php');
+            throw new SOAPException($e->getMessage(), $e->getCode(), 'reportRoomAssignment', $params);
+            return false;
+        }
 
-		if($response->CreateRoomAssignmentResult != "0"){
-			SOAP::logSoap('reportRoomAssignment', 'failed', $username, $term, $building, $room, $meal);
-			PHPWS_Core::initModClass('hms', 'exception/BannerException.php');
-			throw new BannerException('Error while reporting assignment to Banner.', $response->CreateRoomAssignmentResult, 'reportRoomAssignment', $params);
-			return FALSE;
-		}
+        if($response->CreateRoomAssignmentResult != "0"){
+            SOAP::logSoap('reportRoomAssignment', 'failed', $username, $term, $building, $room, $meal);
+            PHPWS_Core::initModClass('hms', 'exception/BannerException.php');
+            throw new BannerException('Error while reporting assignment to Banner.', $response->CreateRoomAssignmentResult, 'reportRoomAssignment', $params);
+            return FALSE;
+        }
 
-		SOAP::logSoap('reportRoomAssignment', 'success', $username, $term, $building, $room, $meal);
-		return true;
-	}
+        SOAP::logSoap('reportRoomAssignment', 'success', $username, $term, $building, $room, $meal);
+        return true;
+    }
 
-	public function removeRoomAssignment($username, $term, $building, $room)
-	{
-		$params = array(
+    public function removeRoomAssignment($username, $term, $building, $room)
+    {
+        $params = array(
                         'StudentID'=>$username,
                         'TermCode'=>$term,
                         'BldgCode'=>$building,
                         'RoomCode'=>$room);
 
-		try{
-			$response = $this->client->RemoveRoomAssignment($params);
-		}catch(SoapFault $e){
-			PHPWS_Core::initModClass('hms', 'exception/SOAPException.php');
-			throw new SOAPException($e->getMessage(), $e->getCode(), 'removeRoomAssignment', $params);
-			return false;
-		}
+        try{
+            $response = $this->client->RemoveRoomAssignment($params);
+        }catch(SoapFault $e){
+            PHPWS_Core::initModClass('hms', 'exception/SOAPException.php');
+            throw new SOAPException($e->getMessage(), $e->getCode(), 'removeRoomAssignment', $params);
+            return false;
+        }
 
-		if($response->RemoveRoomAssignmentResult != "0"){
-			SOAP::logSoap('removeRoomAssignment', 'failed (' . $response->RemoveRoomAssignmentResult . ')', $username, $term, $building, $room);
-			PHPWS_Core::initModClass('hms', 'exception/BannerException.php');
-			throw new BannerException('Error while reporting removal to Banner.', $response->RemoveRoomAssignmentResult, 'removeRoomAssignment', $params);
-			return false;
-		}
+        if($response->RemoveRoomAssignmentResult != "0"){
+            SOAP::logSoap('removeRoomAssignment', 'failed (' . $response->RemoveRoomAssignmentResult . ')', $username, $term, $building, $room);
+            PHPWS_Core::initModClass('hms', 'exception/BannerException.php');
+            throw new BannerException('Error while reporting removal to Banner.', $response->RemoveRoomAssignmentResult, 'removeRoomAssignment', $params);
+            return false;
+        }
 
-		SOAP::logSoap('removeRoomAssignment', 'success', $username, $term, $building, $room);
-		return TRUE;
-	}
+        SOAP::logSoap('removeRoomAssignment', 'success', $username, $term, $building, $room);
+        return TRUE;
+    }
 
-	public function getHousMealRegister($username, $term, $opt)
-	{
-		$params = array(
+    public function getHousMealRegister($username, $term, $opt)
+    {
+        $params = array(
                         'StudentID'=>$username,
                         'TermCode'=>$term,
                         'Option'=>$opt);
 
-		try{
-			$response = $this->client->GetHousMealRegister($params);
-		}catch(SoapFault $e){
-			PHPWS_Core::initModClass('hms', 'exception/SOAPException.php');
-			throw new SOAPException($e->getMessage(), $e->getCode(), 'getHousMealRegister', $params);
-			return false;
-		}
+        try{
+            $response = $this->client->GetHousMealRegister($params);
+        }catch(SoapFault $e){
+            PHPWS_Core::initModClass('hms', 'exception/SOAPException.php');
+            throw new SOAPException($e->getMessage(), $e->getCode(), 'getHousMealRegister', $params);
+            return false;
+        }
 
-		SOAP::logSoap('getHousMealRegister', 'success', $username, $term, $opt);
-		return $response->GetHousMealRegister;
-	}
+        SOAP::logSoap('getHousMealRegister', 'success', $username, $term, $opt);
+        return $response->GetHousMealRegister;
+    }
 
-	public function getBannerIdByBuildingRoom($building, $room, $term)
-	{
-		$params = array(
+    public function getBannerIdByBuildingRoom($building, $room, $term)
+    {
+        $params = array(
                         'BldgCode'=>$building,
                         'RoomCode'=>$room,
                         'TermCode'=>$term);
 
-		try{
-			$response = $this->client->GetBannerIDbyBuildingRoom($params);
-		}catch(SoapFault $e){
-			throw new SOAPException($e->getMessage(), $e->getCode(), 'getBannerIdByBuildingRoom', $params);
-			return false;
-		}
+        try{
+            $response = $this->client->GetBannerIDbyBuildingRoom($params);
+        }catch(SoapFault $e){
+            throw new SOAPException($e->getMessage(), $e->getCode(), 'getBannerIdByBuildingRoom', $params);
+            return false;
+        }
 
-		SOAP::logSoap('getBannerIdByBuildingRoom', 'success', $building, $room, $term);
-		
-		if(isset($response->GetBannerIDbyBuildingRoomResult)){
-			return $response->GetBannerIDbyBuildingRoomResult;
-		}else{
-			return null;
-		}
-	}
+        SOAP::logSoap('getBannerIdByBuildingRoom', 'success', $building, $room, $term);
 
-	public function setHousingWaiver($username, $term)
-	{
-		$params = array(
+        if(isset($response->GetBannerIDbyBuildingRoomResult)){
+            return $response->GetBannerIDbyBuildingRoomResult;
+        }else{
+            return null;
+        }
+    }
+
+    public function setHousingWaiver($username, $term)
+    {
+        $params = array(
                             'StudentID'=>$username,
                             'TermCode'=>$term);
 
-		try{
-			$response = $this->client->SetHousingWaiver($params);
-		}catch(SoapFault $e){
-			throw new SOAPException($e->getMessage(), $e->getCode(), 'setHousingWaiver', $params);
-			return false;
-		}
+        try{
+            $response = $this->client->SetHousingWaiver($params);
+        }catch(SoapFault $e){
+            throw new SOAPException($e->getMessage(), $e->getCode(), 'setHousingWaiver', $params);
+            return false;
+        }
 
-		if($response->SetHousingWaiverResult != "0"){
-			throw new BannerException('Error while setting waiver flag in Banner.', $response->SetHousingWaiverResult, 'setHousingWaiver', $params);
-			return false;
-		}
-		
-		SOAP::logSoap('setHousingWaiver', 'success', $username, $term);
+        if($response->SetHousingWaiverResult != "0"){
+            throw new BannerException('Error while setting waiver flag in Banner.', $response->SetHousingWaiverResult, 'setHousingWaiver', $params);
+            return false;
+        }
 
-		return true;
-	}
+        SOAP::logSoap('setHousingWaiver', 'success', $username, $term);
 
-	public function clearHousingWaiver($username, $term)
-	{
-		$params = array(
+        return true;
+    }
+
+    public function clearHousingWaiver($username, $term)
+    {
+        $params = array(
                             'StudentID'=>$username,
                             'TermCode'=>$term);
 
-		try{
-			$response = $this->client->ClearHousingWaiver($params);
-		}catch(SoapFault $e){
-			throw new SOAPException($e->getMessage(), $e->getCode(), 'clearHousingWaiver', $params);
-			return false;
-		}
+        try{
+            $response = $this->client->ClearHousingWaiver($params);
+        }catch(SoapFault $e){
+            throw new SOAPException($e->getMessage(), $e->getCode(), 'clearHousingWaiver', $params);
+            return false;
+        }
 
-		if($response->ClearHousingWaiverResult != "0"){
-			throw new BannerException('Error while clearing waiver flag in Banner.', $response->ClearHousingWaiverResult, 'clearHousingWaiver', $params);
-			return false;
-		}
-		
-		SOAP::logSoap('clearHousingWaiver', 'success', $username, $term);
+        if($response->ClearHousingWaiverResult != "0"){
+            throw new BannerException('Error while clearing waiver flag in Banner.', $response->ClearHousingWaiverResult, 'clearHousingWaiver', $params);
+            return false;
+        }
 
-		return true;
-	}
+        SOAP::logSoap('clearHousingWaiver', 'success', $username, $term);
+
+        return true;
+    }
 }
 
 ?>
--- class/RecentStudentSearchList.php
+++ class/RecentStudentSearchList.php
@@ -10,106 +10,106 @@
 
 class RecentStudentSearchList {
 
-	private static $instance;
-	const keyName = 'StudentSearchList';
-	const cachTtl = 28800; // seconds to keep cache for (28,800 => 8 hrs)
+    private static $instance;
+    const keyName = 'StudentSearchList';
+    const cachTtl = 28800; // seconds to keep cache for (28,800 => 8 hrs)
 
-	private $searchList;
-	private $globalSearchList;
+    private $searchList;
+    private $globalSearchList;
 
-	private function __construct()
-	{
-		// Load the global list from cache, if it exists
-		if(apc_fetch(self::keyName) !== FALSE){
-			// Make sure we've loaded the Student class first
-			PHPWS_Core::initModClass('hms', 'Student.php');
-			$this->globalSearchList = apc_fetch(self::keyName);
-		}else{
-			$this->globalSearchList = array();
-		}
+    private function __construct()
+    {
+        // Load the global list from cache, if it exists
+        if(apc_fetch(self::keyName) !== FALSE){
+            // Make sure we've loaded the Student class first
+            PHPWS_Core::initModClass('hms', 'Student.php');
+            $this->globalSearchList = apc_fetch(self::keyName);
+        }else{
+            $this->globalSearchList = array();
+        }
 
-		// Load the user unique search list
-		if(apc_fetch(self::keyName . UserStatus::getUsername()) !== FALSE){
-			// Make sure we've loaded the Student class first
-			PHPWS_Core::initModClass('hms', 'Student.php');
-			$this->searchList = apc_fetch(self::keyName . UserStatus::getUsername());
-		}else{
-			$this->searchList = array();
-		}
-	}
+        // Load the user unique search list
+        if(apc_fetch(self::keyName . UserStatus::getUsername()) !== FALSE){
+            // Make sure we've loaded the Student class first
+            PHPWS_Core::initModClass('hms', 'Student.php');
+            $this->searchList = apc_fetch(self::keyName . UserStatus::getUsername());
+        }else{
+            $this->searchList = array();
+        }
+    }
 
-	public static function getInstance()
-	{
-		if(!isset(self::$instance)){
-			self::$instance = new RecentStudentSearchList();
-		}
+    public static function getInstance()
+    {
+        if(!isset(self::$instance)){
+            self::$instance = new RecentStudentSearchList();
+        }
 
-		return self::$instance;
-	}
+        return self::$instance;
+    }
 
-	/**
-	 * Adds a search to the list.
-	 *
-	 * @param Student $student
-	 * @return void
-	 */
-	public function add(Student $student)
-	{
-		// Sanity checking on params
-		if(!isset($student)){
-			throw new InvalidArgumentException('Missing student object');
-		}
+    /**
+     * Adds a search to the list.
+     *
+     * @param Student $student
+     * @return void
+     */
+    public function add(Student $student)
+    {
+        // Sanity checking on params
+        if(!isset($student)){
+            throw new InvalidArgumentException('Missing student object');
+        }
 
-		/*****
-		 * User's List
-		 */
+        /*****
+         * User's List
+        */
 
-		// Search the array to see if this item already exists. If it does,
-		// remove it and then re-index the array.
-		$key = array_search($student, $this->searchList);
-		if($key !== FALSE){
-			// remove it from the array
-			unset($this->searchList[$key]);
-			// re-index the array
-			$this->searchList = array_values($this->searchList);
-		}
+        // Search the array to see if this item already exists. If it does,
+        // remove it and then re-index the array.
+        $key = array_search($student, $this->searchList);
+        if($key !== FALSE){
+            // remove it from the array
+            unset($this->searchList[$key]);
+            // re-index the array
+            $this->searchList = array_values($this->searchList);
+        }
 
-		// Add this item to the top of the list
-		array_unshift($this->searchList, $student);
+        // Add this item to the top of the list
+        array_unshift($this->searchList, $student);
 
-		// Save this list to the cache
-		apc_store(self::keyName . UserStatus::getUsername(), $this->searchList);
+        // Save this list to the cache
+        apc_store(self::keyName . UserStatus::getUsername(), $this->searchList);
 
-		/*****
-		 * Global List
-		 */
-		$key = array_search($student, $this->globalSearchList);
-		if($key !== FALSE){
-			// remove it from the array
-			unset($this->globalSearchList[$key]);
-			// re-index the array
-			$this->globalSearchList = array_values($this->globalSearchList);
-		}
+        /*****
+         * Global List
+        */
+        $key = array_search($student, $this->globalSearchList);
+        if($key !== FALSE){
+            // remove it from the array
+            unset($this->globalSearchList[$key]);
+            // re-index the array
+            $this->globalSearchList = array_values($this->globalSearchList);
+        }
 
-		// Add this item to the top of the list
-		array_unshift($this->globalSearchList, $student);
+        // Add this item to the top of the list
+        array_unshift($this->globalSearchList, $student);
 
-		// Save this list to the cache
-		apc_store(self::keyName, $this->globalSearchList);
-	}
+        // Save this list to the cache
+        apc_store(self::keyName, $this->globalSearchList);
+    }
 
-	/**
-	 * Returns the search list
-	 */
-	public function getList()
-	{
-		return $this->searchList;
-	}
-	
-	public function getGlobalList()
-	{
-		return $this->globalSearchList;
-	}
+    /**
+     * Returns the search list
+     */
+    public function getList()
+    {
+        return $this->searchList;
+    }
+
+    public function getGlobalList()
+    {
+        return $this->globalSearchList;
+    }
 }
 
 ?>
