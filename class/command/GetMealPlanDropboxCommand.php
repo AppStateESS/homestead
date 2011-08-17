3b2f297135b093b2f9dd5064767887348ad1bf95
adam <devnull@localhost>
2011-08-17 13:59:05 -0400

    Ticket 702 Work Completed

--- class/command/GetMealPlanDropboxCommand.php
+++ class/command/GetMealPlanDropboxCommand.php
@@ -7,15 +7,27 @@ class GetMealPlanDropboxCommand extends Command {
     }
 
     public function execute(CommandContext $context){
-        $options = array(BANNER_MEAL_LOW   => 'Low',
+		PHPWS_Core::initModClass('hms', 'Term.php');
+    	
+    	$options = array(BANNER_MEAL_LOW   => 'Low',
                          BANNER_MEAL_STD   => 'Standard',
                          BANNER_MEAL_HIGH  => 'High',
                          BANNER_MEAL_SUPER => 'Super',
+                         BANNER_MEAL_5WEEK => 'Summer (5 Weeks)',
                          BANNER_MEAL_NONE  => 'None'
                         );
         $form = new PHPWS_Form('select_meal');
         $form->addSelect('plan', $options);
-        $form->setMatch('plan', BANNER_MEAL_STD);
+        
+        
+        // If summer term, set default as Summer 5Week
+        $term = Term::getSelectedTerm();
+        if ( (strlen($term) >= 2) && 
+        	 ( (substr($term, -2) == TERM_SUMMER1) || (substr($term, -2) == TERM_SUMMER2) ) ) {
+        	 	$form->setMatch('plan', BANNER_MEAL_5WEEK);
+        	 } else {
+        	 	$form->setMatch('plan', BANNER_MEAL_STD);
+        	 }
 
         echo implode($form->getTemplate());
         exit;
