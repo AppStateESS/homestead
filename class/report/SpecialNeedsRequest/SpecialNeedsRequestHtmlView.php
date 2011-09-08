fb142b382881402d3dd3320fd5bc35246245b0b9
Matt McNaney <matt@tux.appstate.edu>
2011-09-08 14:13:22 -0400

    missing term tag put in

--- class/report/SpecialNeedsRequest/SpecialNeedsRequestHtmlView.php
+++ class/report/SpecialNeedsRequest/SpecialNeedsRequestHtmlView.php
@@ -12,7 +12,7 @@ class SpecialNeedsRequestHtmlView extends ReportHtmlView {
         $this->tpl = $this->report->getSortedRows();
         parent::render();
 
-        $this->tpl['term'] = Term::toString($this->report->getTerm());
+        $this->tpl['TERM'] = Term::toString($this->report->getTerm());
 
         return PHPWS_Template::process($this->tpl, 'hms', 'admin/reports/SpecialNeedsRequest.tpl');
     }
