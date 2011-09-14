5d0d039af54a2a0db7ef825e87233633d08af9a6
jbooker <jeremy.booker@gmail.com>
2011-09-14 16:06:38 -0400

    Code formatting (indenting) fixes.

--- class/AssignStudentView.php
+++ class/AssignStudentView.php
@@ -121,18 +121,18 @@ class AssignStudentView extends View {
 
         // Addition of "Assignment Type"
         $form->addDropBox('assignment_type', array(
-        	ASSIGN_ADMIN		=> 'Administrative',
-        	ASSIGN_LOTTERY		=> 'Lottery',
-        	ASSIGN_FRESHMEN		=> 'Freshmen',
-        	ASSIGN_MEDICAL		=> 'Medical',
-        	ASSIGN_ATHLETICS	=> 'Athletics',
-        	ASSIGN_HONORS		=> 'Honors',
-        	ASSIGN_WATAUGA		=> 'Watauga Global',
-        	ASSIGN_TEACHING		=> 'Teaching Fellows',
-        	ASSIGN_RLC			=> 'RLC',
-        	ASSIGN_SORORITY		=> 'Sorority',
-        	ASSIGN_SPECIAL		=> 'Special Needs',
-        	ASSIGN_AUTO			=> 'Auto-assigned'));
+            ASSIGN_ADMIN        => 'Administrative',
+            ASSIGN_LOTTERY	    => 'Lottery',
+            ASSIGN_FRESHMEN	    => 'Freshmen',
+            ASSIGN_MEDICAL      => 'Medical',
+            ASSIGN_ATHLETICS    => 'Athletics',
+            ASSIGN_HONORS       => 'Honors',
+            ASSIGN_WATAUGA      => 'Watauga Global',
+            ASSIGN_TEACHING     => 'Teaching Fellows',
+            ASSIGN_RLC          => 'RLC',
+            ASSIGN_SORORITY     => 'Sorority',
+            ASSIGN_SPECIAL      => 'Special Needs',
+            ASSIGN_AUTO         => 'Auto-assigned'));
         $form->setMatch('assignment_type', ASSIGN_ADMIN);
         $form->setLabel('assignment_type', 'Assignment Type: ');
         
