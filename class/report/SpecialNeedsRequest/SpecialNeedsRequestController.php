e07631576fdcdc946fc2911379af92a0dbeb08fc
jbooker <jeremy.booker@gmail.com>
2011-09-19 12:50:23 -0400

    Added background support to SpecialNeedsRequest

--- class/report/SpecialNeedsRequest/SpecialNeedsRequestController.php
+++ class/report/SpecialNeedsRequest/SpecialNeedsRequestController.php
@@ -5,7 +5,7 @@
  * @license http://opensource.org/licenses/gpl-3.0.html
  */
 
-class SpecialNeedsRequestController extends ReportController implements iSyncReport, iHtmlReportView, iPdfReportView, iCsvReportView {
+class SpecialNeedsRequestController extends ReportController implements iSyncReport, iAsyncReport, iSchedReport, iHtmlReportView, iPdfReportView, iCsvReportView {
 
     public function setParams(Array $params)
     {
