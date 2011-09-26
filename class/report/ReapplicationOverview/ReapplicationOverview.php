f5364bceea9d6613dd207cfc1ffaedbf8f35e5a3
jbooker <jeremy.booker@gmail.com>
2011-09-26 10:30:21 -0400

    Last few changes to the Reapplication Overview report.

--- class/report/ReapplicationOverview/ReapplicationOverview.php
+++ class/report/ReapplicationOverview/ReapplicationOverview.php
@@ -29,7 +29,18 @@ class ReapplicationOverview extends Report implements iCsvReport {
         
         $lotteryTerm = PHPWS_Settings::get('hms', 'lottery_term');
         
-        $numLotteryEntries = PHPWS_DB::getOne("SELECT count(*) FROM hms_new_application JOIN hms_lottery_application ON hms_new_application.id = hms_lottery_application.id WHERE (hms_new_application.term = $lotteryTerm)");
+        // Number of entries total
+        $this->data['LOTTERY_APPLICATIONS']    = PHPWS_DB::getOne("SELECT count(*) FROM hms_new_application JOIN hms_lottery_application ON hms_new_application.id = hms_lottery_application.id WHERE (hms_new_application.term = $lotteryTerm)");
+        
+        // Number of entries by gender
+        $this->data['LOTTERY_FEMALE_APPLICATIONS'] = PHPWS_DB::getOne("SELECT count(*) FROM hms_new_application JOIN hms_lottery_application ON hms_new_application.id = hms_lottery_application.id WHERE hms_new_application.term = $lotteryTerm AND gender = 1");
+        $this->data['LOTTERY_MALE_APPLICATIONS'] = PHPWS_DB::getOne("SELECT count(*) FROM hms_new_application JOIN hms_lottery_application ON hms_new_application.id = hms_lottery_application.id WHERE hms_new_application.term = $lotteryTerm AND gender = 0");
+        
+        // Number of entires by class
+        $this->data['SOPH_APPLICATIONS']       = HMS_Lottery::count_applications_by_class($lotteryTerm, CLASS_SOPHOMORE);
+        $this->data['JR_APPLICATIONS']         = HMS_Lottery::count_applications_by_class($lotteryTerm, CLASS_JUNIOR);
+        $this->data['SR_APPLICATIONS']         = HMS_Lottery::count_applications_by_class($lotteryTerm, CLASS_SENIOR);
+        
         
         //TODO make this based on lottery assignment reason
         $db = new PHPWS_DB('hms_assignment');
@@ -37,31 +48,32 @@ class ReapplicationOverview extends Report implements iCsvReport {
         $db->addWhere('lottery', 1);
         $numLotteryAssigned = $db->select('count');
         
-        $this->data['LOTTERY_APPLICATIONS']    = $numLotteryEntries;
-        $this->data['SOPH_APPLICATIONS']       = HMS_Lottery::count_applications_by_class($lotteryTerm, CLASS_SOPHOMORE);
-        $this->data['JR_APPLICATIONS']         = HMS_Lottery::count_applications_by_class($lotteryTerm, CLASS_JUNIOR);
-        $this->data['SR_APPLICATIONS']         = HMS_Lottery::count_applications_by_class($lotteryTerm, CLASS_SENIOR);
-        
+        // Assignments
         $this->data['LOTTERY_ASSIGNED']        = $numLotteryAssigned;
+        
+        // Assignments by class
         $this->data['SOPH_ASSIGNED']           = HMS_Lottery::count_assignments_by_class($lotteryTerm, CLASS_SOPHOMORE);
         $this->data['JR_ASSIGNED']             = HMS_Lottery::count_assignments_by_class($lotteryTerm, CLASS_JUNIOR);
         $this->data['SR_ASSIGNED']             = HMS_Lottery::count_assignments_by_class($lotteryTerm, CLASS_SENIOR);
 
-        $this->data['REMAINING_ENTRIES']       = HMS_Lottery::count_remaining_entries($lotteryTerm);
-        $this->data['SOPH_ENTRIES_REMAIN']     = HMS_Lottery::count_remaining_entries_by_class($lotteryTerm, CLASS_SOPHOMORE);
-        $this->data['JR_ENTRIES_REMAIN']       = HMS_Lottery::count_remaining_entries_by_class($lotteryTerm, CLASS_JUNIOR);
-        $this->data['SR_ENTRIES_REMAIN']       = HMS_Lottery::count_remaining_entries_by_class($lotteryTerm, CLASS_SENIOR);
-        
+        // Invites sent
+        $this->data['SOPH_INVITES']            = HMS_Lottery::count_invites_by_class($lotteryTerm, CLASS_SOPHOMORE);
+        $this->data['JR_INVITES']              = HMS_Lottery::count_invites_by_class($lotteryTerm, CLASS_JUNIOR);
+        $this->data['SR_INVITES']              = HMS_Lottery::count_invites_by_class($lotteryTerm, CLASS_SENIOR);
+
+        // Outstanding invites
         $this->data['OUTSTANDING_INVITES']     = HMS_Lottery::count_outstanding_invites($lotteryTerm, MALE) + HMS_Lottery::count_outstanding_invites($lotteryTerm, FEMALE);
         $this->data['SOPH_OUTSTANDING']        = HMS_Lottery::count_outstanding_invites_by_class($lotteryTerm, CLASS_SOPHOMORE);
         $this->data['JR_OUTSTANDING']          = HMS_Lottery::count_outstanding_invites_by_class($lotteryTerm, CLASS_JUNIOR);
         $this->data['SR_OUTSTANDING']          = HMS_Lottery::count_outstanding_invites_by_class($lotteryTerm, CLASS_SENIOR);
-        
+
         $this->data['ROOMMATE_INVITES']        = HMS_Lottery::count_outstanding_roommate_invites($lotteryTerm);
         
-        $this->data['SOPH_INVITES']            = HMS_Lottery::count_invites_by_class($lotteryTerm, CLASS_SOPHOMORE);
-        $this->data['JR_INVITES']              = HMS_Lottery::count_invites_by_class($lotteryTerm, CLASS_JUNIOR);
-        $this->data['SR_INVITES']              = HMS_Lottery::count_invites_by_class($lotteryTerm, CLASS_SENIOR);
+        // Remaining applications
+        $this->data['REMAINING_ENTRIES']       = HMS_Lottery::count_remaining_entries($lotteryTerm);
+        $this->data['SOPH_ENTRIES_REMAIN']     = HMS_Lottery::count_remaining_entries_by_class($lotteryTerm, CLASS_SOPHOMORE);
+        $this->data['JR_ENTRIES_REMAIN']       = HMS_Lottery::count_remaining_entries_by_class($lotteryTerm, CLASS_JUNIOR);
+        $this->data['SR_ENTRIES_REMAIN']       = HMS_Lottery::count_remaining_entries_by_class($lotteryTerm, CLASS_SENIOR);
     }
 
     public function getCsvColumnsArray()
--- templates/admin/reports/ReapplicationOverview.tpl
+++ templates/admin/reports/ReapplicationOverview.tpl
@@ -4,19 +4,34 @@
 
 <table>
   <tr>
-    <td>Lottery applications total:</td>
+    <th colspan="2">Re-applications</th>
+  </tr>
+  <tr>
+    <td>Re-applications total:</td>
     <td>{LOTTERY_APPLICATIONS}</td>
   </tr>
   <tr>
-    <td>Sophomore applications:</td>
+    <td>Male:</td>
+    <td>{LOTTERY_MALE_APPLICATIONS}</td>
+  </tr>
+  <tr>
+    <td>Female:</td>
+    <td>{LOTTERY_FEMALE_APPLICATIONS}</td>
+  </tr>
+
+  <tr>
+    <td colspan="2">&nbsp;</td>
+  </tr>
+  <tr>
+    <td>Sophomore:</td>
     <td>{SOPH_APPLICATIONS}</td>
   </tr>
   <tr>
-    <td>Junior applications:</td>
+    <td>Junior:</td>
     <td>{JR_APPLICATIONS}</td>
   </tr>
   <tr>
-    <td>Senior applications:</td>
+    <td>Senior:</td>
     <td>{SR_APPLICATIONS}</td>
   </tr>
   <tr>
@@ -24,28 +39,23 @@
   </tr>
 
   <tr>
-    <th colspan="2">Re-assignments</th>
+    <th colspan="2">Invites Sent</th>
   </tr>
   <tr>
-    <td>Re-applications assigned:</td>
-    <td>{LOTTERY_ASSIGNED}</td>
+    <td>Sophomore invites sent:</td>
+    <td>{SOPH_INVITES}</td>
   </tr>
   <tr>
-    <td>Sophomores assigned:</td>
-    <td>{SOPH_ASSIGNED}</td>
+    <td>Junior invites sent:</td>
+    <td>{JR_INVITES}</td>
   </tr>
   <tr>
-    <td>Juniors assigned:</td>
-    <td>{JR_ASSIGNED}</td>
-  </tr>
-  <tr>
-    <td>Seniors assigned:</td>
-    <td>{SR_ASSIGNED}</td>
+    <td>Senior invites sent:</td>
+    <td>{SR_INVITES}</td>
   </tr>
   <tr>
     <td colspan="2">&nbsp;</td>
   </tr>
-
 
   <tr>
     <th colspan="2">Outstanding Invites</th>
@@ -74,21 +84,24 @@
     <td colspan="2">&nbsp;</td>
   </tr>
 
-
   <tr>
-    <th colspan="2">Invites Sent</th>
+    <th colspan="2">Re-assignments</th>
   </tr>
   <tr>
-    <td>Sophomore invites sent:</td>
-    <td>{SOPH_INVITES}</td>
+    <td>Re-applications assigned:</td>
+    <td>{LOTTERY_ASSIGNED}</td>
   </tr>
   <tr>
-    <td>Junior invites sent:</td>
-    <td>{JR_INVITES}</td>
+    <td>Sophomores assigned:</td>
+    <td>{SOPH_ASSIGNED}</td>
   </tr>
   <tr>
-    <td>Senior invites sent:</td>
-    <td>{SR_INVITES}</td>
+    <td>Juniors assigned:</td>
+    <td>{JR_ASSIGNED}</td>
+  </tr>
+  <tr>
+    <td>Seniors assigned:</td>
+    <td>{SR_ASSIGNED}</td>
   </tr>
   <tr>
     <td colspan="2">&nbsp;</td>
