3bc3d66603b62f14b409224da9165736d675074d
jbooker <jeremy.booker@gmail.com>
2012-02-20 08:49:52 -0500

    More lottery work.

--- class/command/LotteryShowChooseHallCommand.php
+++ class/command/LotteryShowChooseHallCommand.php
@@ -10,8 +10,16 @@ class LotteryShowChooseHalLCommand extends Command {
     {
         PHPWS_Core::initModClass('hms', 'LotteryChooseHallView.php');
         PHPWS_Core::initModClass('hms', 'StudentFactory.php');
+        PHPWS_Core::initModClass('hms', 'LotteryProcess.php');
         
         $term = PHPWS_Settings::get('hms', 'lottery_term');
+        
+        // Check the hard cap!
+        if(LotteryProcess::hardCapReached($term)){
+            NQ::simple('hms', HMS_NOTIFICATION_ERROR, 'Sorry, re-application is now closed.');
+            $errorCmd = CommandFactory::getCommand('ShowStudentMenu');
+            $errorCmd->redirect();
+        }
         
         $student = StudentFactory::getStudentByUsername(UserStatus::getUsername(), $term);
         
