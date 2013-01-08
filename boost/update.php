53db270ca5d81fa809b0d11a551dba2c96cb6a12
jbooker <jeremy.booker@gmail.com>
2013-01-08 11:52:02 -0500

    Added permissions, updated update script to setup permissions.

--- boost/update.php
+++ boost/update.php
@@ -1370,6 +1370,8 @@ function hms_update(&$content, $currentVersion)
 			if(PEAR::isError($result)){
 				return $result;
 			}
+			PHPWS_Core::initModClass('users', 'Permission.php');
+			Users_Permission::registerPermissions('hms', $content);
 	}
 
 	return TRUE;
