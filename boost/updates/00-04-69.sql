2d0219b45ca66a0c8b90a3c291a239da4380094a
jbooker <jeremy.booker@gmail.com>
2013-01-15 12:07:16 -0500

    Un-merged some dbupdates and pulled them out into their own file.

---
+++ boost/updates/00-04-69.sql
@@ -0,0 +1,6 @@
+delete from hms_student_cache;
+alter table hms_student_cache add column preferred_name character varying;
+alter table hms_student_cache add column confidential character(1) NOT NULL;
+alter table hms_student_cache alter column admissions_decision_code type character varying;
+alter table hms_student_cache add column admissions_decision_desc character varying;
+alter table hms_student_cache add column greek character varying;
