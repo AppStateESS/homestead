8f0d765f02d55bb6f435019de1a9635ca184983d
jbooker <jeremy.booker@gmail.com>
2013-02-04 11:48:35 -0500

    Added permission for RA login as self. Upped version to 0.4.71

--- boost/boost.php
+++ boost/boost.php
@@ -1,6 +1,6 @@
 <?php
     $proper_name  = 'Housing Management System';
-    $version      = '0.4.70';
+    $version      = '0.4.71';
     $import_sql   = TRUE;
     $image_dir    = TRUE;
     $file_dir     = FALSE;
--- class/TestSOAP.php
+++ class/TestSOAP.php
@@ -54,7 +54,7 @@ class TestSOAP extends SOAP{
         //$response->application_term      = '201040';
         //$response->projected_class       = 'FR';
 
-        $response->student_type          = 'F';
+        $response->student_type          = 'C';
         $response->application_term      = '201240';
         $response->projected_class       = 'FR';
 
