8f5f7673550c7c3ed0b6c432e9fab6b582d28b66
jbooker <jeremy.booker@gmail.com>
2013-08-20 17:07:03 -0400

    Fixed bug in check-in history view.

--- class/CheckinHistoryView.php
+++ class/CheckinHistoryView.php
@@ -6,37 +6,43 @@
  * @package hms
  */
 class CheckinHistoryView extends View {
-    
+
     private $checkins;
-    
+
     public function __construct(Array $checkins)
     {
         $this->checkins = $checkins;
     }
-    
+
     public function show()
     {
         $historyRows = array();
-        
+
         foreach ($this->checkins as $checkin) {
             $row = array();
-            
+
             $bed = new HMS_Bed($checkin->getBedId());
-            
+
             $row['room']     = $bed->where_am_i();
             $row['term']     = Term::toString($checkin->getTerm());
             $row['checkin']  = date("M j, Y g:i:sa", $checkin->getCheckinDate());
-            $row['checkout'] = date("", $checkin->getCheckoutDate());
+
+            $checkoutDate = $checkin->getCheckoutDate();
+            if(isset($checkoutDate)){
+                $row['checkout'] = date("M j, Y g:i:sa", $checkoutDate);
+            }else{
+                $row['checkout'] = '';
+            }
 
             $ricCommand = CommandFactory::getCommand('GenerateInfoCard');
             $ricCommand->setCheckinId($checkin->getId());
             $row['action'] = $ricCommand->getLink('Get RIC');
-            
+
             $historyRows[] = $row;
         }
-        
+
         $tpl['HISTORY'] = $historyRows;
-        
+
         return PHPWS_Template::process($tpl, 'hms', 'admin/StudentCheckinHistoryView.tpl');
     }
 }
