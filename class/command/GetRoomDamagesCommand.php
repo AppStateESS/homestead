fa3537cd1398e039f7d5e3b141d7aaed7fc08433
jlbooker <jeremy.booker@gmail.com>
2013-12-06 15:09:06 -0500

    Updating angular views and scripts for checkout damages.

--- class/command/GetRoomDamagesCommand.php
+++ class/command/GetRoomDamagesCommand.php
@@ -9,46 +9,14 @@ class GetRoomDamagesCommand extends Command {
 
     public function execute(CommandContext $context)
     {
-        PHPWS_Core::initModClass('hms', 'StudentFactory.php');
-        PHPWS_Core::initModClass('hms', 'CheckinFactory.php');
         PHPWS_Core::initModClass('hms', 'RoomDamageFactory.php');
-        PHPWS_Core::initModClass('hms', 'HMS_Residence_Hall.php');
         PHPWS_Core::initModClass('hms', 'HMS_Bed.php');
 
         $term = Term::getCurrentTerm();
 
-        $bannerId = $context->get('bannerId');
-        $hallId = $context->get('hallId');
+        $bedId = $context->get('bed_id');
 
-        if (!isset($bannerId) || is_null($bannerId) || $bannerId == '') {
-            // 500
-        }
-
-        if (!isset($hallId)) {
-            // 500
-        }
-
-        // Make sure it looks like a valid Banner ID
-        if (preg_match("/[\d]{9}/", $bannerId) == false) {
-            // 500 - Improperly formatted Banner Id
-        }
-
-        // Try to lookup the student in Banner
-        try {
-            $student = StudentFactory::getStudentByBannerId($bannerId, $term);
-        } catch (StudentNotFoundException $e) {
-            // 500 - Student couldn't be found
-        }
-
-        // Find the earliest checkin that matches hall the user selected
-        $hall = new HMS_Residence_Hall($hallId);
-        $checkin = CheckinFactory::getPendingCheckoutForStudentByHall($student, $hall);
-
-        if(!isset($checkin)){
-            // 500 - Couldn't find a matching checkin
-        }
-
-        $bed = new HMS_Bed($checkin->getBedId());
+        $bed = new HMS_Bed($bedId);
         $room = $bed->get_parent();
 
         // Get the damages for this student's room
--- javascript/AngularFrontend/head.js
+++ javascript/AngularFrontend/head.js
@@ -2,14 +2,15 @@
 
 <script src="{JAVASCRIPT_BASE}/AngularFrontend/scripts/20bdd8fe.plugins.js"></script>
 <script src="{JAVASCRIPT_BASE}/AngularFrontend/scripts/34dfa171.modules.js"></script>
-<script src="{JAVASCRIPT_BASE}/AngularFrontend/scripts/92002d70.scripts.js"></script>
+<script src="{JAVASCRIPT_BASE}/AngularFrontend/scripts/c6b0052f.scripts.js"></script>
 
 <script type="text/javascript">
 
 	(function() {
 		angular.module('roomDamages')
 			.config(function(roomDamageBrokerProvider){
-				roomDamageBrokerProvider.setDamageTypes({DAMAGE_TYPES})
+				roomDamageBrokerProvider.setDamageTypes({DAMAGE_TYPES});
+				roomDamageBrokerProvider.setLocation('http://localhost/hms/phpwebsite/index.php');
 			});
 	})();
 	
