de6654aea606141e8a6e7606233641d54aa67577
jlbooker <jeremy.booker@gmail.com>
2013-12-18 16:28:54 -0500

    Remove old checkout form view.

--- class/CheckoutFormView.php
+++ class/CheckoutFormView.php
@@ -56,68 +56,6 @@ class CheckoutFormView extends View {
         return '<div data-ng-app="hmsAngularApp"><div data-ng-include="\''.$rawfile.'\'"></div></div>';
     }
 
-    public function oldShow()
-    {
-        $dmgTypes = DamageTypeFactory::getDamageTypeAssoc();
-
-        $tpl = array();
-
-        $tpl['NAME']		= $this->student->getName();
-        $tpl['ASSIGNMENT']	= $this->bed->where_am_i();
-        $tpl['BANNER_ID'] 	= $this->student->getBannerId();
-
-        $dmgRows = array();
-
-        if (isset($dmg)) {
-            foreach ($this->damages as $dmg) {
-                $dmgLine = array ();
-                $dmgLine['REPORTED_ON'] = date("M j, Y", $dmg->getReportedOn());
-                $dmgLine['CATEGORY']    = $dmgTypes[$dmg->getDamageType()]['category'];
-                $dmgLine['DESCRIPTION'] = $dmgTypes[$dmg->getDamageType()]['description'];
-                $dmgLine['SIDE']        = $dmg->getSide();
-                $dmgLine['NOTE']        = $dmg->getNote();
-                $dmgRows[] = $dmgLine;
-            }
-        }
-
-        $tpl['damages_repeat'] = $dmgRows;
-
-        // Setup dialog for adding damages
-        $jsParams = array('LINK_SELECT'=>'#addDamageLink');
-        javascript('addRoomDamage', $jsParams, 'mod/hms/');
-
-        $dmgCmd = CommandFactory::getCommand('ShowAddRoomDamage');
-        $dmgCmd->setRoom($this->room);
-        $tpl['ADD_DAMAGE_LINK']  = $dmgCmd->getLink('Add Damage');
-
-        $form = new PHPWS_Form('checkin_form');
-
-        $submitCmd = CommandFactory::getCommand('CheckoutFormSubmit');
-        $submitCmd->setBannerId($this->student->getBannerId());
-
-        $submitCmd->setCheckinId($this->checkin->getId());
-        $submitCmd->initForm($form);
-
-        // Key not returned
-        $form->addCheckAssoc('key_not_returned', array('key_not_returned'=>'Key not returned'));
-
-        // Key code
-        $form->addText('key_code');
-        $form->setLabel('key_code', 'Key Code &#35;');
-        $form->setExtra('key_code', 'autofocus');
-
-        // Improper Checkout
-        $form->addCheckAssoc('improper_checkout', array('improper_checkout'=>'Improper Check-out'));
-
-        $form->addSubmit('submit', 'Continue');
-        $form->setClass('submit', 'btn btn-primary');
-
-        $form->mergeTemplate($tpl);
-        $tpl = $form->getTemplate();
-
-        return PHPWS_Template::process($tpl, 'hms', 'admin/checkoutForm.tpl');
-    }
-
 }
 
 ?>
