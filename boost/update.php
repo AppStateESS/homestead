8315196dd4d161921efb4db1542e4337f36e6a1d
jlbooker <jeremy.booker@gmail.com>
2013-12-19 15:03:57 -0500

    Upped verison to 0.4.82. Added update script to refresh permission list.

--- boost/update.php
+++ boost/update.php
@@ -1438,6 +1438,9 @@ function hms_update(&$content, $currentVersion)
             if (PEAR::isError($result)) {
                 return $result;
             }
+        case version_compare($currentVersion, '0.4.82', '<') :
+            PHPWS_Core::initModClass('users', 'Permission.php');
+            Users_Permission::registerPermissions('hms', $content);
     }
 
     return TRUE;
