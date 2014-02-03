a48dba465cbe7532b63582540a6731af2e80f28f
jlbooker <jeremy.booker@gmail.com>
2014-02-03 09:55:28 -0500

    Improve success message for sending learning community invites.

--- class/command/SendRlcInvitesCommand.php
+++ class/command/SendRlcInvitesCommand.php
@@ -2,26 +2,26 @@
 
 /**
  * SendRlcInvitesComands
- * 
+ *
  * Transitions all new rlc assignments to the 'Invited' state.
- * 
+ *
  * @author jbooker
  * @package HMS
  */
 class SendRlcInvitesCommand extends Command {
-    
+
     public function getRequestVars()
     {
         return array('action'=>'SendRlcInvites');
     }
-    
+
     public function execute(CommandContext $context)
     {
         $resultCmd = CommandFactory::getCommand('ShowSendRlcInvites');
-        
+
         $respondByDate = $context->get('respond_by_date');
         $respondByTime = $context->get('time');
-        
+
         if(!isset($respondByDate) || $respondByDate == ''){
             NQ::simple('hms', HMS_NOTIFICATION_ERROR, 'Please choose a \'respond by\' date.');
             $resultCmd->redirect();
@@ -29,7 +29,7 @@ class SendRlcInvitesCommand extends Command {
 
         $dateParts = explode('/', $respondByDate);
         $respondByTimestamp = mktime($respondByTime, null, null, $dateParts[0], $dateParts[1], $dateParts[2]);
-        
+
         $term = Term::getSelectedTerm();
 
         $studentType = $context->get('type');
@@ -41,19 +41,19 @@ class SendRlcInvitesCommand extends Command {
 
         PHPWS_Core::initModClass('hms', 'RlcAssignmentFactory.php');
         PHPWS_Core::initModClass('hms', 'RlcAssignmentInvitedState.php');
-        
+
         $assignments = RlcAssignmentFactory::getAssignmentsByTermStateType($term, 'new', $studentType);
-        
+
         if(sizeof($assignments) == 0){
             NQ::simple('hms', HMS_NOTIFICATION_WARNING, 'No invites needed to be sent.');
             $resultCmd->redirect();
         }
-        
+
         foreach($assignments as $assign){
             $assign->changeState(new RlcAssignmentInvitedState($assign, $respondByTimestamp));
         }
 
-        NQ::simple('hms', HMS_NOTIFICATION_SUCCESS, 'Freshmen RLC invites sent.');
+        NQ::simple('hms', HMS_NOTIFICATION_SUCCESS, 'Learning community invites sent.');
         $resultCmd->redirect();
     }
 }
