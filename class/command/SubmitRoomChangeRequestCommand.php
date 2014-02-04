5012a000a3ee45e21daaa664584e69a57d6eebf5
jlbooker <jeremy.booker@gmail.com>
2014-02-04 09:49:59 -0500

    Fix undefined variable causing swap notification emails to fail.

--- class/command/SubmitRoomChangeRequestCommand.php
+++ class/command/SubmitRoomChangeRequestCommand.php
@@ -137,7 +137,7 @@ class SubmitRoomChangeRequestCommand extends Command {
                     $participant->setHallPref2($hall);
                 }
             }
-            
+
             // Email the RDs
             foreach($participant->getCurrentRdList() as $rd) {
                 HMS_Email::sendRoomChangeCurrRDNotice($rd, $participant);
@@ -159,7 +159,7 @@ class SubmitRoomChangeRequestCommand extends Command {
             // Send "request needs your approval" to other students
             // TODO: When you add the ability to have many people on this request, you will
             //       need to put a foreach loop here or something.
-            HMS_Email::sendRoomChangeParticipantNotice($swapStudent, $requestor);
+            HMS_Email::sendRoomChangeParticipantNotice($swapStudent, $student);
         }
 
         // Save the main participant and its state
@@ -170,12 +170,8 @@ class SubmitRoomChangeRequestCommand extends Command {
 
         HMS_Activity_Log::log_activity(UserStatus::getUsername(), ACTIVITY_ROOM_CHANGE_SUBMITTED, UserStatus::getUsername(FALSE), $reason);
 
-        // Make a Student object for Requestor, used by email below
-        $requestor = StudentFactory::getStudentByUsername(
-            UserStatus::getUsername(), Term::getCurrentTerm());
-
         // Email sender with acknowledgment
-        HMS_Email::sendRoomChangeRequestAcknowledgment($requestor);
+        HMS_Email::sendRoomChangeRequestAcknowledgment($student);
 
         if ($type == 'switch') {
             NQ::simple('hms', HMS_NOTIFICATION_SUCCESS, 'Your room change request has been received and is pending approval. You will be contacted by your Residence Director (RD) in the next 24-48 hours regarding your request.');
