d24249e63b07ab96bd9061e6d39f9ba1710d288f
jlbooker <jeremy.booker@gmail.com>
2014-02-05 11:12:21 -0500

    Add createRoomUniqueId contrib script.

---
+++ contrib/createRoomUniqueId.php
@@ -0,0 +1,28 @@
+#!/usr/bin/php
+<?php
+
+require_once('cliCommon.php');
+require_once('dbConnect.php');
+
+ini_set('display_errors', 1);
+ini_set('ERROR_REPORTING', E_WARNING);
+error_reporting(E_ALL);
+
+$args = array('term' => '');
+
+$switches = array();
+check_args($argc, $argv, $args, $switches);
+
+$db = connectToDb();
+
+$query = "SELECT * FROM hms_room WHERE term = {$args['term']}"; 
+$result = pg_query($query);
+
+while ($room = pg_fetch_array($result)){ 
+    $sql = "UPDATE hms_room SET persistent_id = '" . uniqid() . "' where id = {$room['id']} and term = {$args['term']}";
+    pg_query($sql);
+}
+
+pg_close($db);
+
+?>
