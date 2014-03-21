1f1f2d604f4d2925d90965a02aec23afe00eafe2
jlbooker <jeremy.booker@gmail.com>
2014-03-21 16:37:02 -0400

    Removed dead code in AssignRlcApplicantsCommand.

--- class/command/AssignRlcApplicantsCommand.php
+++ class/command/AssignRlcApplicantsCommand.php
@@ -15,8 +15,6 @@ class AssignRlcApplicantsCommand extends Command {
             throw new PermissionException('You do not have permission to approve RLC applications.');
         }
 
-        
-        $errors = array();
 
         PHPWS_Core::initModClass('hms','HMS_RLC_Application.php');
         PHPWS_Core::initModClass('hms','HMS_RLC_Assignment.php');
@@ -26,14 +24,14 @@ class AssignRlcApplicantsCommand extends Command {
         # $app_id is the 'id' column in the 'learning_community_applications' table, tells which student we're assigning
         # $rlc_id is the 'id' column in the 'learning_communitites' table, and refers to the RLC selected for the student
         foreach($_REQUEST['final_rlc'] as $app_id => $rlc_id){
-            
+
             if($rlc_id <= 0){
                 continue;
             }
-            
-            $app = HMS_RLC_Application::getApplicationById($app_id);            
+
+            $app = HMS_RLC_Application::getApplicationById($app_id);
             $student = StudentFactory::getStudentByUsername($app->username, $app->term);
-           
+
             # Insert a new assignment in the 'learning_community_assignment' table
             $assign = new HMS_RLC_Assignment();
             $assign->rlc_id         = $rlc_id;
@@ -41,17 +39,17 @@ class AssignRlcApplicantsCommand extends Command {
             $assign->assigned_by    = UserStatus::getUsername();
             $assign->application_id = $app->id;
             $assign->state          = 'new';
-            
+
             $assign->save();
-            
+
             # Log the assignment
             PHPWS_Core::initModClass('hms', 'HMS_Activity_Log.php');
             HMS_Activity_Log::log_activity($app->username, ACTIVITY_ASSIGN_TO_RLC, UserStatus::getUsername(), "New Assignment");
         }
-        
+
         // Show a success message
         NQ::simple('hms', HMS_NOTIFICATION_SUCCESS, 'Successfully assigned RLC applicant(s).');
-        
+
         $context->goBack();
     }
 }
