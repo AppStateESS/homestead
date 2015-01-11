c32d459652d9a00387a8208e18c82d3e8a310d9d
Jeremy Booker <jeremy.booker@gmail.com>
2015-01-10 20:38:15 -0500

    Modify ShowReApplication command to catch users coming back from DocuSign process.

--- class/command/ShowReApplicationCommand.php
+++ class/command/ShowReApplicationCommand.php
@@ -51,10 +51,12 @@ class ShowReApplicationCommand extends Command {
         }
 
         # Make sure the student agreed to the terms, if not, send them back to the terms & agreement command
-        $agreedToTerms = $context->get('agreedToTerms');
+        //$agreedToTerms = $context->get('agreedToTerms');
+        $event = $context->get('event');
 
         # If they haven't agreed, redirect to the agreement
-        if(is_null($agreedToTerms) || !isset($agreedToTerms) || $agreedToTerms != 1){
+        //if(is_null($agreedToTerms) || !isset($agreedToTerms) || $agreedToTerms != 1){
+        if(is_null($event) || !isset($event) || ($event != 'signing_complete' && $event != 'viewing_complete')){
             $onAgree = CommandFactory::getCommand('ShowReApplication');
             $onAgree->setTerm($term);
 
