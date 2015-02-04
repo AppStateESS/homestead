d1183d96f571a8d3b1bdd3a4d1e1739f0828c7b6
Jeremy Booker <jeremy.booker@gmail.com>
2015-02-04 09:38:31 -0500

    Modify update.php to run 00-04-90.sql db update.

--- boost/update.php
+++ boost/update.php
@@ -1458,6 +1458,12 @@ function hms_update(&$content, $currentVersion)
             if (PEAR::isError($result)) {
                 return $result;
             }
+        case version_compare($currentVersion, '0.4.90', '<') :
+            $db = new PHPWS_DB();
+            $result = $db->importFile(PHPWS_SOURCE_DIR . 'mod/hms/boost/updates/00-04-90.sql');
+            if (PEAR::isError($result)) {
+                return $result;
+            }
     }
 
     return TRUE;
