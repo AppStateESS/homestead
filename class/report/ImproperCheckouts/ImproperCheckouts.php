22133aa49a14893d21e10fa04874f57ac33f0845
Jeremy Booker <jeremy.booker@gmail.com>
2015-03-12 11:31:06 -0400

    Update ImproperCheckouts report to add note column. Refs #898

--- class/report/ImproperCheckouts/ImproperCheckouts.php
+++ class/report/ImproperCheckouts/ImproperCheckouts.php
@@ -24,12 +24,12 @@ class ImproperCheckouts extends Report implements iCsvReport {
 
     public function execute()
     {
-        PHPWS_Core::initCoreClass('PdoFactory.php');
+        PHPWS_Core::initModClass('hms', 'PdoFactory.php');
         PHPWS_Core::initModClass('hms', 'StudentFactory.php');
 
         $db = PdoFactory::getInstance()->getPdo();
 
-        $query = "select hall_name, room_number, hms_checkin.banner_id, to_timestamp(checkout_date), checkout_by from hms_checkin JOIN hms_bed ON (hms_checkin.bed_persistent_id = hms_bed.persistent_id AND hms_checkin.term = hms_bed.term) JOIN hms_room ON hms_bed.room_id = hms_room.id JOIN hms_floor ON hms_room.floor_id = hms_floor.id JOIN hms_residence_hall ON hms_floor.residence_hall_id = hms_residence_hall.id WHERE hms_checkin.term = :term and improper_checkout = 1 ORDER BY checkout_date";
+        $query = "select hall_name, room_number, hms_checkin.banner_id, to_timestamp(checkout_date), checkout_by, improper_checkout_note from hms_checkin JOIN hms_bed ON (hms_checkin.bed_persistent_id = hms_bed.persistent_id AND hms_checkin.term = hms_bed.term) JOIN hms_room ON hms_bed.room_id = hms_room.id JOIN hms_floor ON hms_room.floor_id = hms_floor.id JOIN hms_residence_hall ON hms_floor.residence_hall_id = hms_residence_hall.id WHERE hms_checkin.term = :term and improper_checkout = 1 ORDER BY checkout_date";
 
         $stmt = $db->prepare($query);
         $stmt->execute(array('term'=>$this->term));
@@ -57,7 +57,7 @@ class ImproperCheckouts extends Report implements iCsvReport {
 
     public function getCsvColumnsArray()
     {
-        return array('Hall Name', 'Room Number', 'Banner Id', 'Checkout Date', 'Checkout By', 'First Name', 'Last Name');
+        return array('Hall Name', 'Room Number', 'Banner Id', 'Checkout Date', 'Checkout By', 'Note', 'First Name', 'Last Name', 'Username');
     }
 
     public function getCsvRowsArray()
