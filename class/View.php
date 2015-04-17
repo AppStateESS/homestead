11b00ea3f41e692078363bf647b824a9c3c1f927
Jeremy Booker <jeremy.booker@gmail.com>
2015-04-17 15:01:10 -0400

    Fix namespace issue in View.php for Layout class.

--- class/View.php
+++ class/View.php
@@ -14,7 +14,7 @@ abstract class View
 
     public function setTitle($title){
         $this->pageTitle = $title;
-        Layout::addPageTitle($title);
+        \Layout::addPageTitle($title);
     }
 
     public abstract function show();
