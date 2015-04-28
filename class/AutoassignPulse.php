b8e8eac293f5b46f3fb47336b399490d56656f7a
Jeremy Booker <jeremy.booker@gmail.com>
2015-04-28 12:09:23 -0400

    Remove jtickle from autoassign results emails.

--- class/AutoassignPulse.php
+++ class/AutoassignPulse.php
@@ -39,7 +39,6 @@ class AutoassignPulse extends ScheduledPulse
         ob_end_clean();
 
         // TODO: Email the person that scheduled the autoassign.
-        mail('ticklejw@appstate.edu', 'Autoassign Complete', $message);
         mail('jb67803@appstate.edu', 'Autoassign Complete', $message);
 
         return TRUE;
