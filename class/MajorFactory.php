2473613ad886ec5377938a9aaa0848e60d91ae6c
Jeremy Booker <jeremy.booker@gmail.com>
2015-05-11 12:57:49 -0400

    Refactor roommate profiles majors out into MajorFactory. MajorsFactory pulls from a Kimono API now.
    NB: This will likely break the major drop down on existing roommate profiles. #831

---
+++ class/MajorFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace hms;
+
+class MajorFactory {
+
+    public static function getMajorsList()
+    {
+        $url = "https://www.kimonolabs.com/api/cu4u6h42?apikey=5b853442f3f828997cd1a5f0e341e6e1";
+        $json = file_get_contents($url);
+        $results = json_decode($json, TRUE);
+
+        $majors = array();
+        $majors[] = 'Prefer not to say';
+        $majors[] = 'Undecided';
+
+        foreach($results['results']['Major List'] as $item)
+        {
+            if(stristr($item['Major']['text'], 'see ') === FALSE)
+            {
+                $majors[] = $item['Major']['text'];
+            }
+        }
+
+        return $majors;
+    }
+}
