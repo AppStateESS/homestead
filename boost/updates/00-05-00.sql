e10fa730359a8a243fec91c2cab328d837a7bbe3
Jeremy Booker <jeremy.booker@gmail.com>
2015-05-11 12:22:00 -0400

    Add database update script for updated messaging links in student profiles. #876

--- boost/updates/00-05-00.sql
+++ boost/updates/00-05-00.sql
@@ -1,2 +1,12 @@
 alter table hms_student_profiles add column banner_id integer;
 alter table hms_student_profiles add column about_me character varying;
+
+alter table hms_student_profiles drop column aim_sn;
+alter table hms_student_profiles drop column yahoo_sn;
+alter table hms_student_profiles drop column msn_sn;
+
+alter table hms_student_profiles add column fb_link character varying;
+alter table hms_student_profiles add column instagram_sn character varying;
+alter table hms_student_profiles add column twitter_sn character varying;
+alter table hms_student_profiles add column tumblr_sn character varying;
+alter table hms_student_profiles add column kik_sn character varying;
