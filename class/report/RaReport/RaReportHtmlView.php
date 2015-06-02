19ca4f4dc4df308ebacc75c523780ce9ab255115
John Felipe <johnfe423@gmail.com>
2015-06-02 15:53:47 -0400

    Created a report of every RA on campus for the current term refs #901

---
+++ class/report/RaReport/RaReportHtmlView.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * HTML View for Ra Report.
+ *
+ * @author John Felipe
+ * @package HMS
+ */
+
+class RaReportHtmlView extends ReportHtmlView 
+{
+	protected function render()
+    {
+		parent::render();
+		$this->tpl['rows'] = $this->report->getRows();
+
+	    $this->tpl['TERM'] = Term::toString($this->report->getTerm());
+
+	    
+
+	    return PHPWS_Template::process($this->tpl, 'hms', 'admin/reports/RaReport.tpl');
+	}
+}
+
+
+?>
