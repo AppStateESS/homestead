b2f6de2f9aa2786218d878db967059464985fb72
Stardog <mcnaney@gmail.com>
2015-06-04 11:41:56 -0400

    hms_reports directory added in files.
    you know, for all the time we install hms.

--- boost/install.php
+++ boost/install.php
@@ -1,25 +1,29 @@
 <?php
 
-/************************************************************/
+/* * ********************************************************* */
 /* Install file for hms, creates local users for the module */
 /*                                                          */
 /* @author Daniel West                                      */
 /* @package mod                                             */
 /* @subpackage hms                                          */
-/************************************************************/
+/* * ********************************************************* */
 
 function hms_install(&$content)
 {
-   PHPWS_Core::initModClass('users', 'Users.php');
-   $user = new PHPWS_User(); 
-   $user->setUsername('hms_admin');
-   $user->setPassword('in the white room, with black curtains');
-   $user->save();
+    PHPWS_Core::initModClass('users', 'Users.php');
+    $user = new PHPWS_User();
+    $user->setUsername('hms_admin');
+    $user->setPassword('in the white room, with black curtains');
+    $user->save();
 
-   $user = new PHPWS_User();
-   $user->setUsername('hms_student');
-   $user->setPassword('shes my everything, shes my pride and joy');
-   $user->save();
+    $user = new PHPWS_User();
+    $user->setUsername('hms_student');
+    $user->setPassword('shes my everything, shes my pride and joy');
+    $user->save();
 
-   return true;
+    $directory = PHPWS_HOME_DIR . 'files/hms_reports/';
+    if (!is_dir($directory)) {
+        mkdir($directory);
+    }
+    return true;
 }
