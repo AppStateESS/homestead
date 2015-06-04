897912ce4df8a41e131bfb48817df8155a584074
Chris Detsch <cd62936@email.appstate.edu>
2015-06-04 11:14:33 -0400

    Changes to make update work

--- boost/updates/00-05-02.sql
+++ boost/updates/00-05-02.sql
@@ -1,11 +1,15 @@
 alter table hms_fall_application add column smoking_preference smallint;
-alter table hms_fall_application alter column smoking_preference set not null;
+update hms_fall_application set smoking_preference = 0;
 alter table hms_fall_application alter column smoking_preference set default 0;
+alter table hms_fall_application alter column smoking_preference set not null;
+
 
 alter table hms_spring_application add column smoking_preference smallint;
-alter table hms_spring_application alter column smoking_preference set not null;
+update hms_spring_application set smoking_preference = 0;
 alter table hms_spring_application alter column smoking_preference set default 0;
+alter table hms_spring_application alter column smoking_preference set not null;
 
 alter table hms_summer_application add column smoking_preference smallint;
-alter table hms_summer_application alter column smoking_preference set not null;
+update hms_summer_application set smoking_preference = 0;
 alter table hms_summer_application alter column smoking_preference set default 0;
+alter table hms_summer_application alter column smoking_preference set not null;
