36a9c42afc185d7642168e600062196bb3ca7718
Chris Detsch <cd62936@email.appstate.edu>
2015-06-08 10:58:45 -0400

    Added bootstrap to the Cancelled Apps by Reason report.

--- class/report/CancelledAppsByReason/CancelledAppsByReason.php
+++ class/report/CancelledAppsByReason/CancelledAppsByReason.php
@@ -21,29 +21,29 @@ class CancelledAppsByReason extends Report {
     public function execute()
     {
         PHPWS_Core::initModClass('hms', 'HousingApplication.php');
-        
+
         $this->reasons = HousingApplication::getCancellationReasons();
-        
+
         // All students
         $db = new PHPWS_DB('hms_new_application');
-        
+
         $db->addColumn('cancelled_reason');
-        $db->addColumn('id', null, 'myCount', true);
+        $db->addColumn('id', null, 'ount', true);
         $db->addWhere('term', $this->getTerm());
         $db->addWhere('cancelled', 1);
-        
+
         $db->addGroupBy('cancelled_reason');
-        
+
         $this->reasonCounts = $db->select('assoc');
 
         // Freshmen
         $db = new PHPWS_DB('hms_new_application');
         $db->addColumn('cancelled_reason');
-        $db->addColumn('id', null, 'myCount', true);
+        $db->addColumn('id', null, 'count', true);
         $db->addWhere('term', $this->getTerm());
         $db->addWhere('cancelled', 1);
         $db->addWhere('student_type', TYPE_FRESHMEN);
-        
+
         $db->addGroupBy('cancelled_reason');
 
         $this->freshmenReasonCounts = $db->select('assoc');
@@ -51,21 +51,21 @@ class CancelledAppsByReason extends Report {
         // Continuing
         $db = new PHPWS_DB('hms_new_application');
         $db->addColumn('cancelled_reason');
-        $db->addColumn('id', null, 'myCount', true);
+        $db->addColumn('id', null, 'count', true);
         $db->addWhere('term', $this->getTerm());
         $db->addWhere('cancelled', 1);
         $db->addWhere('student_type', TYPE_CONTINUING);
-        
+
         $db->addGroupBy('cancelled_reason');
-        
+
         $this->continuingReasonCounts = $db->select('assoc');
     }
-    
+
     public function getReasonCounts()
     {
         return $this->reasonCounts;
     }
-   
+
     public function getFreshmenReasonCounts()
     {
         return $this->freshmenReasonCounts;
@@ -80,12 +80,12 @@ class CancelledAppsByReason extends Report {
     {
         return $this->reasons;
     }
-    
+
     public function setTerm($term)
     {
         $this->term = $term;
     }
-    
+
     public function getTerm()
     {
         return $this->term;
--- class/report/CancelledAppsByReason/CancelledAppsByReasonHtmlView.php
+++ class/report/CancelledAppsByReason/CancelledAppsByReasonHtmlView.php
@@ -2,49 +2,49 @@
 
 /**
  * HTLM view for Cancelled Housing Applications by Reason report.
- * 
+ *
  * @author Jeremy Booker
  * @package HMS
  */
 
 class CancelledAppsByReasonHtmlView extends ReportHtmlView {
-    
+
     protected function render()
     {
         parent::render();
-        
+
         $this->tpl['TERM'] = Term::toString($this->report->getTerm());
         $reasons = $this->report->getReasons();
 
         // All Students
         $rows = array();
-        
+
         $totalCancellations = 0;
 
         foreach($this->report->getReasonCounts() as $reason=>$count){
             $row = array();
-            
-            $row['REASON'] = $reasons[$reason];
-            $row['COUNT']  = $count;
-            
+
+            $row['ALL_REASON'] = $reasons[$reason];
+            $row['ALL_COUNT']  = $count;
             $rows[] = $row;
-            
+
             $totalCancellations += $count;
         }
-        
+
         $this->tpl['TABLE_ROWS'] = $rows;
-        
+
+
         $this->tpl['TOTAL_CANCELLATIONS'] = $totalCancellations;
 
-        
+
         // Freshmen
         $freshmenRows = array();
         $freshmenCancellations = 0;
 
         foreach($this->report->getFreshmenReasonCounts() as $reason=>$count){
             $row = array();
-            $row['REASON'] = $reasons[$reason];
-            $row['COUNT'] = $count;
+            $row['FR_REASON'] = $reasons[$reason];
+            $row['FR_COUNT'] = $count;
 
             $freshmenRows[] = $row;
             $freshmenCancellations += $count;
@@ -60,8 +60,8 @@ class CancelledAppsByReasonHtmlView extends ReportHtmlView {
 
         foreach($this->report->getContinuingReasonCounts() as $reason=>$count){
             $row = array();
-            $row['REASON'] = $reasons[$reason];
-            $row['COUNT'] = $count;
+            $row['C_REASON'] = $reasons[$reason];
+            $row['C_COUNT'] = $count;
 
             $continuingRows[] = $row;
             $continuingCancellations += $count;
@@ -69,6 +69,7 @@ class CancelledAppsByReasonHtmlView extends ReportHtmlView {
 
         $this->tpl['CONTINUING_ROWS'] = $continuingRows;
         $this->tpl['CONTINUING_TOTAL'] = $continuingCancellations;
+
 
         return PHPWS_Template::process($this->tpl, 'hms', 'admin/reports/CancelledAppsByReason.tpl');
     }
