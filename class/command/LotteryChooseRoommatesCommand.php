51d0775623cc4d12e3c81bb1c396ff3594ae1660
Chris Detsch <cd62936@email.appstate.edu>
2016-02-05 17:05:18 -0500

    Reworking the LotterChoosRoommatesCommand to use session vars to save the values before going to docusign.

--- class/command/LotteryChooseRoommatesCommand.php
+++ class/command/LotteryChooseRoommatesCommand.php
@@ -3,14 +3,21 @@
 class LotteryChooseRoommatesCommand extends Command {
 
     private $roomId;
+    private $term;
+
 
     public function setRoomId($id){
         $this->roomId = $id;
     }
 
+    public function setTerm($term)
+    {
+        $this->term = $term;
+    }
+
     public function getRequestVars()
     {
-        return array('action'=>'LotteryChooseRoommates', 'roomId'=>$this->roomId);
+            return array('action'=>'LotteryChooseRoommates', 'roomId'=>$this->roomId);
     }
 
     public function execute(CommandContext $context)
@@ -19,17 +26,20 @@ class LotteryChooseRoommatesCommand extends Command {
         PHPWS_Core::initModClass('hms', 'HousingApplication.php');
         PHPWS_Core::initModClass('hms', 'HMS_Assignment.php');
         PHPWS_Core::initModClass('hms', 'RlcMembershipFactory.php');
-        
+
         $roommates = $context->get('roommates');
         $mealPlan = $context->get('meal_plan');
         $term = PHPWS_Settings::get('hms', 'lottery_term');
 
         $student = StudentFactory::getStudentByUsername(UserStatus::getUsername(), $term);
-        
+
         // Check for an RLC assignment in the self-select status
         $rlcAssignment = RlcMembershipFactory::getMembership($student, $term);
 
         $roomId = $context->get('roomId');
+
+        unset($_SESSION['application_data']);
+        var_dump($_SESSION['application_data']);exit;
 
         if(!isset($roomId) || is_null($roomId) || empty($roomId)){
             throw new InvalidArgumentException('Missing room id.');
@@ -46,7 +56,7 @@ class LotteryChooseRoommatesCommand extends Command {
 
         $errorCmd = CommandFactory::getCommand('LotteryShowChooseRoommates');
         $errorCmd->setRoomId($roomId);
-         
+
         // Make sure the student assigned his/her self to a bed
         if(!in_array(UserStatus::getUsername(), $roommates)){
             NQ::simple('hms', hms\NotificationView::ERROR, 'You must assign yourself to a bed. Please try again.');
@@ -73,7 +83,7 @@ class LotteryChooseRoommatesCommand extends Command {
                 NQ::simple('hms', hms\NotificationView::ERROR, "$roommate is not a valid user name. Please try again.");
                 $errorCmd->redirect();
             }
-            
+
             $bannerId = $studentObj->getBannerId();
 
             // Make sure every user name is a valid student
@@ -86,7 +96,7 @@ class LotteryChooseRoommatesCommand extends Command {
              * We can't check the student type here, because we're working in the future with students who are possibly still considered freshmen (which will always show up as type F)
              * What we can do is make sure their application term is less than the lottery term
              */
-            
+
             $roommateAppTerm = $studentObj->getApplicationTerm();
             if(!isset($roommateAppTerm) || is_null($roommateAppTerm) || empty($roommateAppTerm)){
                 NQ::simple('hms', hms\NotificationView::ERROR, "The Housing Management System does not have complete student data for $roommate. Please select a different roommate.");
@@ -114,7 +124,7 @@ class LotteryChooseRoommatesCommand extends Command {
                     NQ::simple('hms', hms\NotificationView::ERROR, "$roommate must be a member of the same learning community as you, and must also be eligible for self-selction.");
                     $errorCmd->redirect();
                 }
-                
+
             // Otherwise (if not RLC members), make sure each roommate entered the lottery and has a valid application (not cancelled)
             }else if(HousingApplication::checkForApplication($roommate, $term) === FALSE){
                 NQ::simple('hms', hms\NotificationView::ERROR, "$roommate did not re-apply for housing. Please select a different roommate.");
@@ -133,7 +143,27 @@ class LotteryChooseRoommatesCommand extends Command {
                 $errorCmd->redirect();
             }
         }
-        
+
+        // Session the current application data
+        $_SESSION['application_data'] = $_REQUEST;
+
+        # Make sure the student agreed to the terms, if not, send them back to the terms & agreement command
+        $event = $context->get('event');
+
+        # If they haven't agreed, redirect to the agreement
+        if(is_null($event) || !isset($event) || ($event != 'signing_complete' && $event != 'viewing_complete')){
+            $onAgree = CommandFactory::getCommand('LotteryChooseRoommates');
+
+            $agreementCmd = CommandFactory::getCommand('ShowTermsAgreement');
+            $agreementCmd->setTerm($term);
+            $agreementCmd->setAgreedCommand($onAgree);
+            $agreementCmd->redirect();
+        }
+        else {
+
+        }
+
+
         // If we've made it this far, then everything is ok.. redirect to the confirmation screen
         $confirmCmd = CommandFactory::getCommand('LotteryShowConfirm');
         $confirmCmd->setRoomId($roomId);
@@ -142,6 +172,3 @@ class LotteryChooseRoommatesCommand extends Command {
         $confirmCmd->redirect();
     }
 }
-
-
-
