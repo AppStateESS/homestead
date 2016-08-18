209e57e1a3589a4285b63b798cb93d0d9972b016
Jeremy Booker <jeremy.booker@gmail.com>
2016-08-18 10:35:17 -0400

    Use PDO instead of database class in CheckoutList report. Refs #1084

--- class/report/CheckoutList/CheckoutList.php
+++ class/report/CheckoutList/CheckoutList.php
@@ -32,34 +32,14 @@ class CheckoutList extends Report implements iCsvReport {
 
     public function execute()
     {
-        $term = $this->term;
+        $db = PdoFactory::getPdoInstance();
 
+        $query = "SELECT hms_checkin.banner_id, checkout_date, checkout_by, hall_name, floor_number, room_number, bedroom_label, bed_letter FROM hms_checkin JOIN hms_hall_structure ON hms_checkin.bed_id = hms_hall_structure.bedid WHERE hms_checkin.term = :term AND hms_checkin.checkout_date IS NOT NULL ORDER BY hall_name ASC,room_number ASC";
 
-        $db = new PHPWS_DB('hms_checkin');
+        $stmt = $db->prepare($query);
+        $stmt->execute(array('term' => $this->term));
 
-        // Join hall structure
-        $db->addJoin('', 'hms_checkin', 'hms_hall_structure', 'bed_id', 'bedid');
-
-        $db->addColumn('hms_checkin.banner_id');
-        $db->addColumn('hms_checkin.checkout_date');
-        $db->addColumn('hms_checkin.checkout_by');
-        $db->addColumn('hms_hall_structure.hall_name');
-        $db->addColumn('hms_hall_structure.floor_number');
-        $db->addColumn('hms_hall_structure.room_number');
-        $db->addColumn('hms_hall_structure.bedroom_label');
-        $db->addColumn('hms_hall_structure.bed_letter');
-
-        $db->addWhere('hms_checkin.term', $term);
-        $db->addWhere('hms_checkin.checkout_date', null, 'IS NOT');
-
-        // Sort by hall, then room number
-        $db->addOrder(array('hms_hall_structure.hall_name ASC', 'hms_hall_structure.room_number ASC'));
-
-        $results = $db->select();
-
-        if(PHPWS_Error::isError($results)){
-            throw new DatabaseException($results->toString());
-        }
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
         // Post-processing, cleanup, making it pretty
         foreach($results as $row){
