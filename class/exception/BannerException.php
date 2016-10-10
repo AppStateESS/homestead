33e2502cc1f6260b1243ea4f6ca418941910b444
Jeremy Booker <jeremy.booker@gmail.com>
2016-10-10 16:07:21 -0400

    Handle arrays in $params for BannerException class.

--- class/exception/BannerException.php
+++ class/exception/BannerException.php
@@ -6,8 +6,7 @@ class BannerException extends HMSException {
 
     public function __construct($message, $code = 0, $functionName, $params){
         parent::__construct($message, $code);
-        $errorMsg = $functionName . '(' . implode(',', $params) . '): ' . $code;
+        $errorMsg = $functionName . ' ' . $code . ' ' . print_r($params, true);
         PHPWS_Core::log($errorMsg, 'soapError.log', ('BannerError'));
     }
 }
-
