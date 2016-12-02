0b8e8e7ed4637f1e179625bce5c05602f7fc7016
Jeremy Booker <jeremy.booker@gmail.com>
2016-12-02 16:13:29 -0500

    Add initial version of DocuSign mass status update script.

--- contrib/updateDocusignContractStatus.php
+++ contrib/updateDocusignContractStatus.php
@@ -1,26 +1,29 @@
 #!/usr/bin/php
 <?php
 
-require_once('mod/hms/contrib/cliCommon.php');
-require_once('mod/hms/contrib/dbConnect.php');
-
-require_once 'config/core/config.php';
-//require_once 'src/Bootstrap.php';
-require_once 'src/Autoloader.php';
-
-require_once 'mod/hms/vendor/autoload.php';
-require_once 'mod/hms/class/DocusignClientFactory.php';
-require_once 'mod/hms/class/Docusign/Client.php';
-
 ini_set('display_errors', 1);
 ini_set('ERROR_REPORTING', E_WARNING);
 error_reporting(E_ALL);
 
-$args = array('term' => '');
+require_once('cliCommon.php');
+
+$args = array('phpwsPath' => '',
+                'term' => '');
 $switches = array();
 check_args($argc, $argv, $args, $switches);
 
+$phpwsPath = $args['phpwsPath'];
 $term = $args['term'];
+
+require_once($phpwsPath . 'mod/hms/contrib/dbConnect.php');
+
+require_once $phpwsPath . 'config/core/config.php';
+//require_once 'src/Bootstrap.php';
+require_once $phpwsPath . 'src/Autoloader.php';
+
+require_once $phpwsPath . 'mod/hms/vendor/autoload.php';
+require_once $phpwsPath . 'mod/hms/class/DocusignClientFactory.php';
+require_once $phpwsPath . 'mod/hms/class/Docusign/Client.php';
 
 $db = connectToDb();
 
@@ -28,35 +31,91 @@ if(!$db){
     die('Could not connect to database.\n');
 }
 
-$sql = "select envelope_id from hms_contract where term = {$term}";
+$docusignClient = DocusignClientFactory::getClient();
+$http = new \Guzzle\Http\Client();
+
+
+// Locate all the contracts we need to check, make a list of them
+$sql = "select envelope_id from hms_contract where term = {$term} and envelope_status NOT IN ('completed', 'voided')";
 $result = pg_query($sql);
 
+// Fetch each row and add it to a list
 $envelopeIdList = array();
-
 while($row = pg_fetch_array($result)){
     $envelopeIdList[] = $row['envelope_id'];
 }
 
-$envelopeIdJson = json_encode(array('envelope_ids' => $envelopeIdList));
-echo $envelopeIdJson . "\n\n";
+$envCount = sizeof($envelopeIdList);
 
-$docusignClient = DocusignClientFactory::getClient();
-
-$http = new \Guzzle\Http\Client();
-$url = $docusignClient->getBaseUrl() . '/envelopes/status';
-echo $url . "\n\n";
-$request = $http->createRequest('PUT', $url);
-$request->setBody($envelopeIdJson, 'application/json');
-$request->setHeader('Content-Type', 'application/json');
-$request->setHeader('Accept', 'application/json');
-$request->setHeader('X-DocuSign-Authentication', $docusignClient->getAuthHeader());
-
-try{
-    $response = $http->send($request);
-    $result = $response->json();
-}catch (\Guzzle\Http\Exception\BadResponseException $e){
-    throw new \Exception(print_r($e->getResponse()->json(), true));
+if($envCount === 0){
+    echo "No envelopes need to be udpated.\n";
+    exit;
+} else {
+    echo "Found $envCount envelopes to check\n";
 }
 
-var_dump($result);
+
+// Lookup 25 contracts at a time, until there are no more remaining
+$offset = 0;
+$envelopesPerRequest = 25;
+while($offset < $envCount){
+    echo "Requesting status for $envelopesPerRequest starting at offset $offset... \n";
+
+    $envelopeSlice = array_slice($envelopeIdList, $offset, $envelopesPerRequest);
+
+    $envelopes = sendRequest($envelopeSlice, $docusignClient, $http);
+    saveEnvelopes($envelopes);
+
+    $offset += $envelopesPerRequest;
+
+    // Wait at least another second before we get the next batch
+    sleep(1);
+}
 echo "\n\n";
+
+
+function sendRequest(Array $envelopeIdList, $docusignClient, $guzzleClient) {
+    $envelopeIdJson = json_encode(array('envelopeIds' => $envelopeIdList));
+
+    $url = $docusignClient->getBaseUrl() . '/envelopes/status?envelope_ids=request_body';
+    //echo $url . "\n\n";
+    $request = $guzzleClient->createRequest('PUT', $url);
+    $request->setBody($envelopeIdJson, 'application/json');
+    $request->setHeader('Content-Type', 'application/json');
+    $request->setHeader('Accept', 'application/json');
+    $request->setHeader('X-DocuSign-Authentication', $docusignClient->getAuthHeader());
+
+    try{
+        $response = $guzzleClient->send($request);
+        $result = $response->json();
+    }catch (\Guzzle\Http\Exception\BadResponseException $e){
+        throw new \Exception(print_r($e->getResponse()->json(), true));
+    }
+
+    if(!isset($result['envelopes']) || sizeof($result['envelopes']) == 0){
+        // We didn't get anything back, so we should probably stop here
+        echo "No 'envelopes' key in the response, or the key had size == 0. Quitting.\n\n";
+        exit;
+    }
+
+    return $result['envelopes'];
+}
+
+function saveEnvelopes(Array $envelopes){
+    // For each envelope we got back, try save the new data to our db
+    foreach($envelopes as $env){
+        // Update the database with each envelope's new status and date
+        $status = $env['status'];
+        $time = strtotime($env['statusChangedDateTime']);
+        $envelopeId = $env['envelopeId'];
+
+        echo "Updating $envelopeId...\n";
+        $sql = "UPDATE hms_contract SET envelope_status = '$status', envelope_status_time = $time where envelope_id = '$envelopeId'";
+        $result = pg_query($sql);
+
+        if($result === false){
+            echo "Failed to update envelopeId: $enveloepId\n";
+            continue;
+        }
+    }
+}
