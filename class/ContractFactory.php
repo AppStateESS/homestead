dd8f007e9189c2e0d7f1e2ac42ec814a4f3a195d
Jeremy Booker <jeremy.booker@gmail.com>
2017-04-03 14:42:02 -0400

    Fix code indention in ContractFactory.php

--- class/ContractFactory.php
+++ class/ContractFactory.php
@@ -8,13 +8,13 @@ class ContractFactory {
 
     public static function getContractById($id)
     {
-    	//TODO
+        // TODO
     }
 
     public static function getContractByStudentTerm(Student $student, $term)
     {
         PHPWS_Core::initModClass('hms', 'Contract.php');
-    		$db = PdoFactory::getPdoInstance();
+        $db = PdoFactory::getPdoInstance();
 
         $query = 'SELECT * FROM hms_contract WHERE banner_id = :bannerId AND term = :term';
 
@@ -30,53 +30,53 @@ class ContractFactory {
 
     public static function getContractsForStudent(Student $student)
     {
-    	// TODO
+        // TODO
     }
 
     public static function save($contract)
     {
-    	$db = PdoFactory::getPdoInstance();
+        $db = PdoFactory::getPdoInstance();
 
-      $id = $contract->getId();
+        $id = $contract->getId();
 
-      if (isset($id)) {
-        $query = "UPDATE hms_contract
-                    SET (banner_id, term, envelope_id, envelope_status, envelope_status_time) =
-                    (:bannerId, :term, :envelopeId, :envelopeStatus, :envelopeStatusTime) WHERE id = :id";
+        if (isset($id)) {
+            $query = "UPDATE hms_contract
+                SET (banner_id, term, envelope_id, envelope_status, envelope_status_time) =
+                (:bannerId, :term, :envelopeId, :envelopeStatus, :envelopeStatusTime) WHERE id = :id";
 
-        $params = array(
-                  'id' => $contract->getId(),
-                  'bannerId' => $contract->getBannerId(),
-                  'term' => $contract->getTerm(),
-                  'envelopeId' => $contract->getEnvelopeId(),
-                  'envelopeStatus' => $contract->getEnvelopeStatus(),
-                  'envelopeStatusTime' => $contract->getEnvelopeStatusTime()
-      	);
+            $params = array(
+                'id' => $contract->getId(),
+                'bannerId' => $contract->getBannerId(),
+                'term' => $contract->getTerm(),
+                'envelopeId' => $contract->getEnvelopeId(),
+                'envelopeStatus' => $contract->getEnvelopeStatus(),
+                'envelopeStatusTime' => $contract->getEnvelopeStatusTime()
+            );
 
-      }else{
-        // Insert
-        $query = "INSERT INTO hms_contract (id, banner_id, term, envelope_id, envelope_status, envelope_status_time) VALUES (nextval('hms_contract_seq'), :bannerId, :term, :envelopeId, :envelopeStatus, :envelopeStatusTime)";
+        }else{
+            // Insert
+            $query = "INSERT INTO hms_contract (id, banner_id, term, envelope_id, envelope_status, envelope_status_time) VALUES (nextval('hms_contract_seq'), :bannerId, :term, :envelopeId, :envelopeStatus, :envelopeStatusTime)";
 
-        $params = array(
-                  'bannerId' => $contract->getBannerId(),
-                  'term' => $contract->getTerm(),
-                  'envelopeId' => $contract->getEnvelopeId(),
-                  'envelopeStatus' => $contract->getEnvelopeStatus(),
-                  'envelopeStatusTime' => $contract->getEnvelopeStatusTime()
-        );
-      }
+            $params = array(
+                'bannerId' => $contract->getBannerId(),
+                'term' => $contract->getTerm(),
+                'envelopeId' => $contract->getEnvelopeId(),
+                'envelopeStatus' => $contract->getEnvelopeStatus(),
+                'envelopeStatusTime' => $contract->getEnvelopeStatusTime()
+            );
+        }
 
-      //var_dump($params);
-      //var_dump($query);exit;
+        //var_dump($params);
+        //var_dump($query);exit;
 
-      $stmt = $db->prepare($query);
-      $stmt->execute($params);
+        $stmt = $db->prepare($query);
+        $stmt->execute($params);
 
-      //var_dump($db->errorInfo());exit;
+        //var_dump($db->errorInfo());exit;
 
-      // Update ID for a new object
-      if (!isset($id)) {
-        $contract->setId($db->lastInsertId('hms_contract_seq'));
-      }
+        // Update ID for a new object
+        if (!isset($id)) {
+            $contract->setId($db->lastInsertId('hms_contract_seq'));
+        }
     }
 }
