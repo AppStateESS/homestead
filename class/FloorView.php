da44b7838ad33386cc9cb73b657cf3fda23ad514
Jeremy Booker <jeremy.booker@gmail.com>
2017-10-18 20:37:01 -0400

    Fix handling of disabled form elements in FloorView

--- class/FloorView.php
+++ class/FloorView.php
@@ -112,8 +112,7 @@ class FloorView extends hms\View{
         && !Current_User::allow('hms', 'floor_attributes')
         && !Current_User::allow('hms', 'floor_structure'))
         {
-            $form_vars = get_object_vars($form);
-            $elements = $form_vars['_elements'];
+            $elements = $form->getAllElements()
 
             foreach($elements as $element => $value){
                 $form->setDisabled($element);
