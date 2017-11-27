5b7878b13f2b10effec342ffdedfc176d4d3cbf0
tylercraig9332 <tmancraig03@gmail.com>
2017-11-27 16:31:46 -0500

    Fixed an issue with JQuery timepicker not functioning properly

--- javascript/timePicker/jquery.timePicker.js
+++ javascript/timePicker/jquery.timePicker.js
@@ -18,6 +18,45 @@
  *   show24Hours: use a 24-hour scheme
  */
 
+/*
+The code below is the $.browser intitalization, since the .browser method was removed in the newest jquery update
+*/
+ var matched, browser;
+
+ jQuery.uaMatch = function( ua ) {
+     ua = ua.toLowerCase();
+
+     var match = /(chrome)[ \/]([\w.]+)/.exec( ua ) ||
+         /(webkit)[ \/]([\w.]+)/.exec( ua ) ||
+         /(opera)(?:.*version|)[ \/]([\w.]+)/.exec( ua ) ||
+         /(msie) ([\w.]+)/.exec( ua ) ||
+         ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec( ua ) ||
+         [];
+
+     return {
+         browser: match[ 1 ] || "",
+         version: match[ 2 ] || "0"
+     };
+ };
+
+ matched = jQuery.uaMatch( navigator.userAgent );
+ browser = {};
+
+ if ( matched.browser ) {
+     browser[ matched.browser ] = true;
+     browser.version = matched.version;
+ }
+
+ // Chrome is Webkit, but Webkit is also Safari.
+ if ( browser.chrome ) {
+     browser.webkit = true;
+ } else if ( browser.webkit ) {
+     browser.safari = true;
+ }
+
+ jQuery.browser = browser;
+
+
 (function($){
   $.fn.timePicker = function(options) {
     // Build main options before element iteration
