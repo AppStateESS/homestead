11010ccb277f459374e16b3a02608f3acf19aa21
Ted Eberhard <eberhardtm@users.noreply.github.com>
2019-02-18 14:03:04 -0500

    Update BeginDocusignCommand.php

--- class/Command/BeginDocusignCommand.php
+++ class/Command/BeginDocusignCommand.php
@@ -158,8 +158,6 @@ class BeginDocusignCommand extends Command
 	$mealPlan = $context->get('mealPlan');
 	if(empty($mealPlan) && !empty($context->get('meal_plan'))){
 		$mealPlan = $context->get('meal_plan');
-	}else{
-		$mealPlan = '1';
 	}
 
         if(isset($mealPlan) && $mealPlan !== null){
